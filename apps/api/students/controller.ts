4f545615292546d3cf54b6b852a3bcfc4f4b7787
nomorechokedboy <haiduongit@protonmail.com>
2025-07-26 14:21:07 +0700

    feat: impl update student controller

--- apps/api/students/controller.ts
+++ apps/api/students/controller.ts
@@ -4,6 +4,7 @@ import {
         StudentParam,
         StudentQuery,
         Student,
+        UpdateStudentMap,
 } from '../schema/student.js';
 import { Repository } from './index.js';
 import studentRepo from './repo.js';
@@ -11,26 +12,51 @@ import studentRepo from './repo.js';
 export class Controller {
         constructor(private repo: Repository) {}
 
-        async create(params: StudentParam[]): Promise<StudentDB[]> {
-                const createdStudent = await this.repo
-                        .create(params)
-                        .catch(AppError.handleAppErr);
-
-                return createdStudent;
+        create(params: StudentParam[]): Promise<StudentDB[]> {
+                return this.repo.create(params).catch(AppError.handleAppErr);
         }
 
-        async delete(students: StudentDB[]) {
-                return await this.repo
-                        .delete(students)
-                        .catch(AppError.handleAppErr);
+        delete(students: StudentDB[]) {
+                return this.repo.delete(students).catch(AppError.handleAppErr);
         }
 
-        async find(q: StudentQuery): Promise<Student[]> {
-                const resp = await await this.repo
-                        .find(q)
-                        .catch(AppError.handleAppErr);
+        find(q: StudentQuery): Promise<Student[]> {
+                return this.repo.find(q).catch(AppError.handleAppErr);
+        }
 
-                return resp;
+        async update(params: StudentDB[]): Promise<StudentDB[]> {
+                const ids = params.map((s) => s.id);
+                const isIdsEmpty = ids.length === 0;
+                const isIdsValid = !ids || isIdsEmpty;
+                if (isIdsValid) {
+                        throw AppError.invalidArgument(
+                                'No record IDs provided'
+                        );
+                }
+
+                const updateMap: UpdateStudentMap = params.map(
+                        ({ id, ...updatePayload }) => {
+                                const cleanupPayload = Object.fromEntries(
+                                        Object.entries(updatePayload).filter(
+                                                ([_, value]) =>
+                                                        value !== undefined
+                                        )
+                                );
+
+                                const isUpdatePayloadEmpty =
+                                        Object.keys(cleanupPayload).length ===
+                                        0;
+                                if (isUpdatePayloadEmpty) {
+                                        throw AppError.invalidArgument(
+                                                `No update data provided At least one field must be provided to update record with id: ${id}`
+                                        );
+                                }
+
+                                return { id, updatePayload: cleanupPayload };
+                        }
+                );
+
+                return this.repo.update(updateMap).catch(AppError.handleAppErr);
         }
 }
 
