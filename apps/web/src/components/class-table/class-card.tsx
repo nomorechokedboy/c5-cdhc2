a0f83c7bd04a7c4485d9d28e3b0630ce7093d5ee
nomorechokedboy <haiduongit@protonmail.com>
2025-07-28 15:07:57 +0700

    feat: class card will link to class detail

--- apps/web/src/components/class-table/class-card.tsx
+++ apps/web/src/components/class-table/class-card.tsx
@@ -7,6 +7,7 @@ import {
 } from '@/components/ui/card';
 import type { Class } from '@/types';
 import { EllipsisText } from '@/components/data-table/ellipsis-text';
+import { Link } from '@tanstack/react-router';
 
 interface ClassCardProps {
         data: Class;
@@ -15,25 +16,27 @@ interface ClassCardProps {
 
 export default function ClassCard({ data, index }: ClassCardProps) {
         return (
+                <Link to={`/classes/$classId`} params={(prev) => ({ ...prev, classId: data.id })}>
                 <Card className="@container/card">
                         <CardHeader>
                                 <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
                                         {data.name}
                                 </CardTitle>
-                                <EllipsisText>
-                                        <CardDescription>
+                                <CardDescription>
+                                        <EllipsisText>
                                                 {data.description}
-                                        </CardDescription>
-                                </EllipsisText>
+                                        </EllipsisText>
+                                </CardDescription>
                         </CardHeader>
                         <CardFooter className="flex-col items-start gap-1.5 text-sm">
                                 <div className="line-clamp-1 flex gap-2 font-medium">
-                                        Tổng số học viên: 50
+                                        Tổng số học viên: {data.studentCount}
                                 </div>
                                 <div className="text-muted-foreground">
-                                        Tạo ngày: 25/12/2024
+                                        Tạo ngày: {data.createdAt}
                                 </div>
                         </CardFooter>
                 </Card>
+                </Link>
         );
 }
