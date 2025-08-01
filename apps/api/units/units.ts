92f9f7145efa3d00a510adff8e8294d663c497b7
nomorechokedboy <haiduongit@protonmail.com>
2025-08-01 09:25:39 +0700

    feat(units): impl units api

---
+++ apps/api/units/units.ts
@@ -0,0 +1,57 @@
+import { api } from 'encore.dev/api'
+import { UnitParams } from '../schema'
+import unitController from './controller'
+
+type UnitBody = {
+	alias: string
+	name: string
+	level: 'battalion' | 'company'
+
+	parentId?: number | null
+}
+
+type UnitDB = UnitBody & { id: number; createdAt: string; updatedAt: string }
+
+interface CreateUnitRequest {
+	data: Array<UnitBody>
+}
+
+interface CreateUnitResponse {
+	data: Array<UnitDB>
+}
+
+export const CreateUnit = api(
+	{ expose: true, method: 'POST', path: '/units' },
+	async (body: CreateUnitRequest): Promise<CreateUnitResponse> => {
+		const unitParams: Array<UnitParams> = body.data.map((u) => ({
+			...u
+		}))
+
+		const createdUnits = await unitController.create(unitParams)
+
+		const resp = createdUnits.map((u) => ({ ...u }) as UnitDB)
+
+		return { data: resp }
+	}
+)
+
+type unit = Omit<UnitBody, 'parentId'>
+
+type Unit = unit & {
+	parent: unit | null
+	children: Unit[]
+}
+
+interface GetUnitsResponse {
+	data: Array<Unit>
+}
+
+export const GetUnits = api(
+	{ expose: true, method: 'GET', path: '/units' },
+	async (): Promise<GetUnitsResponse> => {
+		const resp = await unitController.find()
+		const data = resp.map((u) => ({ ...u }) as Unit)
+
+		return { data }
+	}
+)
