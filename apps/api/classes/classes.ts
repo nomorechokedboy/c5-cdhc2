8d5eaf7f61d9a9a4bc9620b43d9b26cfa3446a2d
nomorechokedboy <haiduongit@protonmail.com>
2025-08-01 10:42:44 +0700

    feat(classes): adjust classes api fields

--- apps/api/classes/classes.ts
+++ apps/api/classes/classes.ts
@@ -1,57 +1,68 @@
-import { api } from 'encore.dev/api';
-import log from 'encore.dev/log';
-import { ClassParam } from '../schema/classes.js';
-import classController from './controller.js';
+import { api } from 'encore.dev/api'
+import log from 'encore.dev/log'
+import { ClassParam } from '../schema/classes.js'
+import classController from './controller.js'
 
 interface ClassBody {
-        name: string;
-        description?: string;
+	name: string
+	description?: string
+
+	graduatedAt?: string
+	unitId: number
 }
 
-interface ClassResponse {
-        id: number;
-        name: string;
-        description: string;
+export interface ClassResponse {
+	id: number
+	createdAt: string
+	updatedAt: string
+
+	name: string
+	description: string
+
+	graduatedAt: string | null
+	status: 'ongoing' | 'graduated'
+
+	unitId: number
 }
 
 interface BulkClassResponse {
-        data: ClassResponse[];
+	data: ClassResponse[]
 }
 
 export const CreateClass = api(
-        { expose: true, method: 'POST', path: '/classes' },
-        async (body: ClassBody): Promise<BulkClassResponse> => {
-                const classParam: ClassParam = {
-                        ...body,
-                };
-                log.trace('classes.CreateClass body', { classParam });
+	{ expose: true, method: 'POST', path: '/classes' },
+	async (body: ClassBody): Promise<BulkClassResponse> => {
+		const classParam: ClassParam = {
+			...body
+		}
+		log.trace('classes.CreateClass body', { classParam })
 
-                const createdClass = await classController.create([classParam]);
+		const createdClass = await classController.create([classParam])
 
-                const resp = createdClass.map(
-                        (c) =>
-                                ({
-                                        ...c,
-                                }) as ClassResponse
-                );
+		const resp = createdClass.map(
+			(c) =>
+				({
+					...c
+				}) as ClassResponse
+		)
 
-                return { data: resp };
-        }
-);
+		return { data: resp }
+	}
+)
 
 interface GetClassesResponse extends BulkClassResponse {}
 
 export const GetClasses = api(
-        { expose: true, method: 'GET', path: '/classes' },
-        async (): Promise<GetClassesResponse> => {
-                const classes = await classController.find();
-                const resp = classes.map(
-                        (c) =>
-                                ({
-                                        ...c,
-                                }) as ClassResponse
-                );
+	{ expose: true, method: 'GET', path: '/classes' },
+	async (): Promise<GetClassesResponse> => {
+		const classes = await classController.find()
+		const resp = classes.map(
+			(c) =>
+				({
+					...c
+				}) as ClassResponse
+		)
 
-                return { data: resp };
-        }
-);
+		return { data: resp }
+	}
+)
