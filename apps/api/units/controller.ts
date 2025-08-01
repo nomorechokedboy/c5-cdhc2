5c1844558bf92dc65d9f82436adf5c6cf8c69ee5
nomorechokedboy <haiduongit@protonmail.com>
2025-08-01 20:24:15 +0700

    feat(units): check unit and parent level

--- apps/api/units/controller.ts
+++ apps/api/units/controller.ts
@@ -1,14 +1,41 @@
+import log from 'encore.dev/log'
 import { Repository } from '.'
 import { AppError } from '../errors'
-import { Unit, UnitParams, UnitQuery } from '../schema'
+import { Unit, UnitLevel, UnitParams, UnitQuery } from '../schema'
 import unitRepo from './repo'
 import { GetUnitsQuery } from './units'
 
 class controller {
 	constructor(private readonly repo: Repository) {}
 
-	create(params: UnitParams[]): Promise<UnitParams[]> {
-		return this.repo.create(params).catch(AppError.handleAppErr)
+	async create(params: UnitParams[]): Promise<UnitParams[]> {
+		log.trace('UnitController.create params', { params })
+
+		const validParams: UnitParams[] = []
+		for (const param of params) {
+			const parent = await this.repo.findOne({ id: param.parentId! })
+			const parentLevel = UnitLevel.fromName(parent!.level)
+			const paramLevel = UnitLevel.fromName(param.level)
+			if (UnitLevel.isEqual(parentLevel, paramLevel)) {
+				throw AppError.handleAppErr(
+					AppError.invalidArgument(
+						`parent level and unit level can't be the same. Parent level:${parent?.level} - Unit level: ${param.level}`
+					)
+				)
+			}
+
+			if (UnitLevel.isLargerThan(paramLevel, parentLevel)) {
+				throw AppError.handleAppErr(
+					AppError.invalidArgument(
+						`unit level can't be higher than parent. Parent level: ${parent?.level} - Unit level: ${param.level}`
+					)
+				)
+			}
+
+			validParams.push(param)
+		}
+
+		return this.repo.create(validParams).catch(AppError.handleAppErr)
 	}
 
 	find(q: GetUnitsQuery): Promise<Unit[]> {
