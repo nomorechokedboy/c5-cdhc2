059502eb439b16ae6c1e821d6b7a8baa97e461a3
nomorechokedboy <haiduongit@protonmail.com>
2025-08-02 19:31:03 +0700

    chore(web): remove unused code

--- apps/web/src/api/index.ts
+++ apps/web/src/api/index.ts
@@ -15,48 +15,6 @@ import type {
 } from '@/types'
 import axios from 'axios'
 
-export type City = {
-	id: number
-	name: string
-	type: number
-	textType: string
-	slug: string
-}
-
-export type CitytResponse = {
-	total: number
-	data: City[]
-	code: 'success' | 'failed'
-	message?: string
-}
-
-export function fromCitiesToSelectValues(
-	cities: City[]
-): { value: string; label: string }[] {
-	return cities.map(({ name }) => ({ value: name, label: name }))
-}
-
-export function GetCities() {
-	return axios
-		.get<CitytResponse>('https://open.oapi.vn/location/provinces?size=63')
-		.then((res) => res.data.data)
-}
-
-export type Ethnic = {
-	id: number
-	name: string
-}
-
-export type EthnicsResponse = Ethnic[]
-
-export function GetEthnics() {
-	return axios
-		.get<EthnicsResponse>(
-			'http://api.nosomovo.xyz/ethnic/getalllist?_gl=1*hsf3mt*_ga*MTI5MjcwNDAzOS4xNzUyODAyOTU3*_ga_XW6CMNCYH8*czE3NTI4MDI5NTckbzEkZzEkdDE3NTI4MDI5NjIkajU1JGwwJGgw'
-		)
-		.then((res) => res.data)
-}
-
 export function CreateClass(body: ClassBody) {
 	return axios
 		.post<ClassResponse>('http://localhost:4000/classes', body)
