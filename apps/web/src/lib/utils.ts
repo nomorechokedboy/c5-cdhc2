5ff3c5de12bd6f875c4e3f29c62263a751af472b
nomorechokedboy <haiduongit@protonmail.com>
2025-08-03 17:18:01 +0700

    feat(utils): add format time helper

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/vi'
+
+dayjs.locale('vi')
+dayjs.extend(relativeTime)
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs))
+}
+
+export const formatTimestamp = (timestamp: string) => {
+	return dayjs(timestamp).fromNow()
 }
