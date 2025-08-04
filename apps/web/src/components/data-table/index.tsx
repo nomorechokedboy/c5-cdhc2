1ff6b084b42273358aae8a4eed3a90567bd72bbf
nomorechokedboy <haiduongit@protonmail.com>
2025-08-04 20:08:34 +0700

    feat(web): add data table placeholder

--- apps/web/src/components/data-table/index.tsx
+++ apps/web/src/components/data-table/index.tsx
@@ -1,242 +1,199 @@
 import {
-        type ColumnDef,
-        type ColumnFiltersState,
-        type SortingState,
-        type VisibilityState,
-        flexRender,
-        getCoreRowModel,
-        getFacetedRowModel,
-        getFacetedUniqueValues,
-        getFilteredRowModel,
-        getPaginationRowModel,
-        getSortedRowModel,
-        useReactTable,
-} from '@tanstack/react-table';
+	type ColumnDef,
+	type ColumnFiltersState,
+	type SortingState,
+	type VisibilityState,
+	flexRender,
+	getCoreRowModel,
+	getFacetedRowModel,
+	getFacetedUniqueValues,
+	getFilteredRowModel,
+	getPaginationRowModel,
+	getSortedRowModel,
+	useReactTable
+} from '@tanstack/react-table'
 import {
-        Table,
-        TableBody,
-        TableCell,
-        TableHead,
-        TableHeader,
-        TableRow,
-} from '@/components/ui/table';
-import { DataTablePagination } from './data-table-pagination';
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow
+} from '@/components/ui/table'
+import { DataTablePagination } from './data-table-pagination'
 import {
-        DataTableToolbar,
-        type DataTableToolbarProps,
-} from './data-table-toolbar';
-import { ComponentType, ReactNode, useState } from 'react';
+	DataTableToolbar,
+	type DataTableToolbarProps
+} from './data-table-toolbar'
+import { type ComponentType, useState } from 'react'
 
-type ToolbarProps<TData> = Omit<DataTableToolbarProps<TData>, 'table'>;
+type ToolbarProps<TData> = Omit<DataTableToolbarProps<TData>, 'table'>
 
 interface DataTableProps<TData, TValue> {
-        cardClassName?: string;
-        cardComponent?: ComponentType<{ data: TData; index: number }>;
-        columns: ColumnDef<TData, TValue>[];
-        data: TData[];
-        defaultColumnVisibility?: VisibilityState;
-        defaultViewMode?: ViewMode;
-        toolbarProps?: ToolbarProps<TData>;
-        tableClassName?: string;
-        pagination?: boolean;
-        toolbarVisible?: boolean;
+	cardClassName?: string
+	cardComponent?: ComponentType<{ data: TData; index: number }>
+	columns: ColumnDef<TData, TValue>[]
+	data: TData[]
+	defaultColumnVisibility?: VisibilityState
+	defaultViewMode?: ViewMode
+	toolbarProps?: ToolbarProps<TData>
+	tableClassName?: string
+	pagination?: boolean
+	toolbarVisible?: boolean
+	placeholder?: string
 }
 
-type ViewMode = 'table' | 'card';
+type ViewMode = 'table' | 'card'
 
 export function DataTable<TData, TValue>({
-        cardClassName = '',
-        cardComponent: CardComponent,
-        columns,
-        data,
-        defaultColumnVisibility = {},
-        defaultViewMode = 'table',
-        toolbarProps,
-        tableClassName,
-        pagination = true,
-        toolbarVisible = true,
+	cardClassName = '',
+	cardComponent: CardComponent,
+	columns,
+	data,
+	defaultColumnVisibility = {},
+	defaultViewMode = 'table',
+	toolbarProps,
+	tableClassName,
+	pagination = true,
+	toolbarVisible = true,
+	placeholder = 'Không có dữ liệu nào'
 }: DataTableProps<TData, TValue>) {
-        const [rowSelection, setRowSelection] = useState({});
-        const [columnVisibility, setColumnVisibility] =
-                useState<VisibilityState>(defaultColumnVisibility);
-        const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(
-                []
-        );
-        const [sorting, setSorting] = useState<SortingState>([]);
-        const [viewMode, setViewMode] = useState<ViewMode>(defaultViewMode);
+	const [rowSelection, setRowSelection] = useState({})
+	const [columnVisibility, setColumnVisibility] = useState<VisibilityState>(
+		defaultColumnVisibility
+	)
+	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+	const [sorting, setSorting] = useState<SortingState>([])
+	const [viewMode, setViewMode] = useState<ViewMode>(defaultViewMode)
 
-        const table = useReactTable({
-                data,
-                columns,
-                state: {
-                        sorting,
-                        columnVisibility,
-                        rowSelection,
-                        columnFilters,
-                },
-                enableRowSelection: true,
-                onRowSelectionChange: setRowSelection,
-                onSortingChange: setSorting,
-                onColumnFiltersChange: setColumnFilters,
-                onColumnVisibilityChange: setColumnVisibility,
-                getCoreRowModel: getCoreRowModel(),
-                getFilteredRowModel: getFilteredRowModel(),
-                getPaginationRowModel: getPaginationRowModel(),
-                getSortedRowModel: getSortedRowModel(),
-                getFacetedRowModel: getFacetedRowModel(),
-                getFacetedUniqueValues: getFacetedUniqueValues(),
-        });
+	const table = useReactTable({
+		data,
+		columns,
+		state: {
+			sorting,
+			columnVisibility,
+			rowSelection,
+			columnFilters
+		},
+		enableRowSelection: true,
+		onRowSelectionChange: setRowSelection,
+		onSortingChange: setSorting,
+		onColumnFiltersChange: setColumnFilters,
+		onColumnVisibilityChange: setColumnVisibility,
+		getCoreRowModel: getCoreRowModel(),
+		getFilteredRowModel: getFilteredRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+		getFacetedRowModel: getFacetedRowModel(),
+		getFacetedUniqueValues: getFacetedUniqueValues()
+	})
 
-        const renderTableView = () => {
-                return (
-                        <div className={`rounded-md border ${tableClassName}`}>
-                                <Table>
-                                        <TableHeader>
-                                                {table
-                                                        .getHeaderGroups()
-                                                        .map((headerGroup) => (
-                                                                <TableRow
-                                                                        key={
-                                                                                headerGroup.id
-                                                                        }
-                                                                >
-                                                                        {headerGroup.headers.map(
-                                                                                (
-                                                                                        header
-                                                                                ) => {
-                                                                                        return (
-                                                                                                <TableHead
-                                                                                                        key={
-                                                                                                                header.id
-                                                                                                        }
-                                                                                                        colSpan={
-                                                                                                                header.colSpan
-                                                                                                        }
-                                                                                                >
-                                                                                                        {header.isPlaceholder
-                                                                                                                ? null
-                                                                                                                : flexRender(
-                                                                                                                          header
-                                                                                                                                  .column
-                                                                                                                                  .columnDef
-                                                                                                                                  .header,
-                                                                                                                          header.getContext()
-                                                                                                                  )}
-                                                                                                </TableHead>
-                                                                                        );
-                                                                                }
-                                                                        )}
-                                                                </TableRow>
-                                                        ))}
-                                        </TableHeader>
-                                        <TableBody>
-                                                {table.getRowModel().rows
-                                                        ?.length ? (
-                                                        table
-                                                                .getRowModel()
-                                                                .rows.map(
-                                                                        (
-                                                                                row
-                                                                        ) => (
-                                                                                <TableRow
-                                                                                        key={
-                                                                                                row.id
-                                                                                        }
-                                                                                        data-state={
-                                                                                                row.getIsSelected() &&
-                                                                                                'selected'
-                                                                                        }
-                                                                                >
-                                                                                        {row
-                                                                                                .getVisibleCells()
-                                                                                                .map(
-                                                                                                        (
-                                                                                                                cell
-                                                                                                        ) => (
-                                                                                                                <TableCell
-                                                                                                                        key={
-                                                                                                                                cell.id
-                                                                                                                        }
-                                                                                                                >
-                                                                                                                        {flexRender(
-                                                                                                                                cell
-                                                                                                                                        .column
-                                                                                                                                        .columnDef
-                                                                                                                                        .cell,
-                                                                                                                                cell.getContext()
-                                                                                                                        )}
-                                                                                                                </TableCell>
-                                                                                                        )
-                                                                                                )}
-                                                                                </TableRow>
-                                                                        )
-                                                                )
-                                                ) : (
-                                                        <TableRow>
-                                                                <TableCell
-                                                                        colSpan={
-                                                                                columns.length
-                                                                        }
-                                                                        className="h-24 text-center"
-                                                                >
-                                                                        No
-                                                                        results.
-                                                                </TableCell>
-                                                        </TableRow>
-                                                )}
-                                        </TableBody>
-                                </Table>
-                        </div>
-                );
-        };
+	const renderTableView = () => {
+		return (
+			<div className={`rounded-md border ${tableClassName}`}>
+				<Table>
+					<TableHeader>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<TableRow key={headerGroup.id}>
+								{headerGroup.headers.map((header) => {
+									return (
+										<TableHead
+											key={header.id}
+											colSpan={header.colSpan}
+										>
+											{header.isPlaceholder
+												? null
+												: flexRender(
+														header.column.columnDef
+															.header,
+														header.getContext()
+													)}
+										</TableHead>
+									)
+								})}
+							</TableRow>
+						))}
+					</TableHeader>
+					<TableBody>
+						{table.getRowModel().rows?.length ? (
+							table.getRowModel().rows.map((row) => (
+								<TableRow
+									key={row.id}
+									data-state={
+										row.getIsSelected() && 'selected'
+									}
+								>
+									{row.getVisibleCells().map((cell) => (
+										<TableCell key={cell.id}>
+											{flexRender(
+												cell.column.columnDef.cell,
+												cell.getContext()
+											)}
+										</TableCell>
+									))}
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								<TableCell
+									colSpan={columns.length}
+									className='h-24 text-center'
+								>
+									No results.
+								</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+			</div>
+		)
+	}
 
-        const renderCardView = () => {
-                if (!CardComponent) {
-                        return (
-                                <div className="text-center py-8 text-muted-foreground">
-                                        No card component provided
-                                </div>
-                        );
-                }
+	const renderCardView = () => {
+		if (!CardComponent) {
+			return (
+				<div className='text-center py-8 text-muted-foreground'>
+					No card component provided
+				</div>
+			)
+		}
 
-                const rows = table.getRowModel().rows;
+		const rows = table.getRowModel().rows
 
-                if (!rows?.length) {
-                        return (
-                                <div className="text-center py-8 text-muted-foreground">
-                                        Không có học sinh nào!
-                                </div>
-                        );
-                }
+		if (!rows?.length) {
+			return (
+				<div className='text-center py-8 text-muted-foreground'>
+					{placeholder}
+				</div>
+			)
+		}
 
-                return (
-                        <div
-                                className={`grid gap-4 ${cardClassName || 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3'}`}
-                        >
-                                {rows.map((row, index) => (
-                                        <CardComponent
-                                                key={row.id}
-                                                data={row.original}
-                                                index={index}
-                                        />
-                                ))}
-                        </div>
-                );
-        };
+		return (
+			<div
+				className={`grid gap-4 ${cardClassName || 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3'}`}
+			>
+				{rows.map((row, index) => (
+					<CardComponent
+						key={row.id}
+						data={row.original}
+						index={index}
+					/>
+				))}
+			</div>
+		)
+	}
 
-        return (
-                <div className="space-y-4">
-                        {toolbarVisible && (
-                                <DataTableToolbar
-                                        table={table}
-                                        onViewModeChange={setViewMode}
-                                        {...toolbarProps}
-                                />
-                        )}
-                        {viewMode === 'table'
-                                ? renderTableView()
-                                : renderCardView()}
-                        {pagination && <DataTablePagination table={table} />}
-                </div>
-        );
+	return (
+		<div className='space-y-4'>
+			{toolbarVisible && (
+				<DataTableToolbar
+					table={table}
+					onViewModeChange={setViewMode}
+					{...toolbarProps}
+				/>
+			)}
+			{viewMode === 'table' ? renderTableView() : renderCardView()}
+			{pagination && <DataTablePagination table={table} />}
+		</div>
+	)
 }
