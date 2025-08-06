3b081def6ab22cb26edc9c389d4c1e5aa9c81805
nomorechokedboy <haiduongit@protonmail.com>
2025-08-06 16:43:21 +0700

    feat(web): adjust student form
    add new date field name cpvOfficialAt

--- apps/web/src/components/personal-step.tsx
+++ apps/web/src/components/personal-step.tsx
@@ -1,173 +1,132 @@
-import {
-        EhtnicOptions,
-        religionOptions,
-        eduLevelOptions,
-} from '@/data/ethnics';
-import useClassData from '@/hooks/useClasses';
-import { useMemo } from 'react';
+import { EhtnicOptions, religionOptions, eduLevelOptions } from '@/data/ethnics'
+import useClassData from '@/hooks/useClasses'
+import { useMemo } from 'react'
 
 export default function PersonalStep({ form }: { form: any }) {
-        const { data: classes = [], refetch } = useClassData();
-        const classOptions = useMemo(
-                () =>
-                        classes.map((c) => ({
-                                value: c.id.toString(),
-                                label: c.name,
-                        })),
-                [classes]
-        );
+	const { data: classes = [], refetch } = useClassData()
+	const classOptions = useMemo(
+		() =>
+			classes.map((c) => ({
+				value: c.id.toString(),
+				label: `${c.name} - ${c.unit.name}`
+			})),
+		[classes]
+	)
 
-        return (
-                <div className="space-y-6">
-                        {/* Full Name - Full Width */}
-                        <div className="grid grid-cols-3 gap-6">
-                                <form.AppField
-                                        name="fullName"
-                                        validators={{
-                                                onBlur: ({
-                                                        value,
-                                                }: {
-                                                        value: string;
-                                                }) => {
-                                                        if (
-                                                                !value ||
-                                                                value.trim()
-                                                                        .length ===
-                                                                        0
-                                                        ) {
-                                                                return 'Họ và tên không được để trống';
-                                                        }
-                                                        return undefined;
-                                                },
-                                        }}
-                                >
-                                        {(field: any) => (
-                                                <field.TextField
-                                                        className="col-span-2"
-                                                        label="Họ và tên"
-                                                />
-                                        )}
-                                </form.AppField>
+	return (
+		<div className='space-y-6'>
+			{/* Full Name - Full Width */}
+			<div className='grid grid-cols-3 gap-6'>
+				<form.AppField
+					name='fullName'
+					validators={{
+						onBlur: ({ value }: { value: string }) => {
+							if (!value || value.trim().length === 0) {
+								return 'Họ và tên không được để trống'
+							}
+							return undefined
+						}
+					}}
+				>
+					{(field: any) => (
+						<field.TextField
+							className='col-span-2'
+							label='Họ và tên'
+						/>
+					)}
+				</form.AppField>
 
-                                <form.AppField name="classId">
-                                        {(field: any) => (
-                                                <field.Select
-                                                        values={classOptions}
-                                                        label="Lớp"
-                                                        // defaultValue={
-                                                        //         eduLevelOptions[0]
-                                                        //                 .value
-                                                        // }
-                                                />
-                                        )}
-                                </form.AppField>
-                        </div>
+				<form.AppField name='classId'>
+					{(field: any) => (
+						<field.Select
+							values={classOptions}
+							label='Lớp'
+							// defaultValue={
+							//         eduLevelOptions[0]
+							//                 .value
+							// }
+						/>
+					)}
+				</form.AppField>
+			</div>
 
-                        {/* Birth Place and Address - Two Columns */}
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                                <form.AppField name="birthPlace">
-                                        {(field: any) => (
-                                                <field.TextField label="Quê quán" />
-                                        )}
-                                </form.AppField>
+			{/* Birth Place and Address - Two Columns */}
+			<div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+				<form.AppField name='birthPlace'>
+					{(field: any) => <field.TextField label='Quê quán' />}
+				</form.AppField>
 
-                                <form.AppField name="address">
-                                        {(field: any) => (
-                                                <field.TextField label="Trú quán" />
-                                        )}
-                                </form.AppField>
-                        </div>
+				<form.AppField name='address'>
+					{(field: any) => <field.TextField label='Trú quán' />}
+				</form.AppField>
+			</div>
 
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                                <form.AppField name="ethnic">
-                                        {(field: any) => (
-                                                <field.Select
-                                                        values={EhtnicOptions}
-                                                        label="Dân tộc"
-                                                        defaultValue={
-                                                                eduLevelOptions[0]
-                                                                        .value
-                                                        }
-                                                />
-                                        )}
-                                </form.AppField>
+			<div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+				<form.AppField name='ethnic'>
+					{(field: any) => (
+						<field.Select
+							values={EhtnicOptions}
+							label='Dân tộc'
+							defaultValue={eduLevelOptions[0].value}
+						/>
+					)}
+				</form.AppField>
 
-                                <form.AppField name="religion">
-                                        {(field: any) => (
-                                                <field.Select
-                                                        values={religionOptions}
-                                                        label="Tôn giáo"
-                                                        defaultValue={
-                                                                religionOptions[0]
-                                                                        .value
-                                                        }
-                                                />
-                                        )}
-                                </form.AppField>
-                        </div>
+				<form.AppField name='religion'>
+					{(field: any) => (
+						<field.Select
+							values={religionOptions}
+							label='Tôn giáo'
+							defaultValue={religionOptions[0].value}
+						/>
+					)}
+				</form.AppField>
+			</div>
 
-                        {/* School Name and Major - Two Columns */}
-                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                                <form.AppField name="educationLevel">
-                                        {(field: any) => (
-                                                <field.Select
-                                                        label="Trình độ học vấn"
-                                                        values={eduLevelOptions}
-                                                        defaultValue={
-                                                                eduLevelOptions[5]
-                                                                        .value
-                                                        }
-                                                />
-                                        )}
-                                </form.AppField>
-                                <form.AppField name="schoolName">
-                                        {(field: any) => (
-                                                <field.TextField label="Tên trường" />
-                                        )}
-                                </form.AppField>
+			{/* School Name and Major - Two Columns */}
+			<div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+				<form.AppField name='educationLevel'>
+					{(field: any) => (
+						<field.Select
+							label='Trình độ học vấn'
+							values={eduLevelOptions}
+							defaultValue={eduLevelOptions[5].value}
+						/>
+					)}
+				</form.AppField>
+				<form.AppField name='schoolName'>
+					{(field: any) => <field.TextField label='Tên trường' />}
+				</form.AppField>
 
-                                <form.AppField name="major">
-                                        {(field: any) => (
-                                                <field.TextField label="Ngành" />
-                                        )}
-                                </form.AppField>
-                        </div>
+				<form.AppField name='major'>
+					{(field: any) => <field.TextField label='Ngành' />}
+				</form.AppField>
+			</div>
 
-                        {/* Phone - Full Width */}
-                        <div className="grid grid-cols-2 gap-6">
-                                <form.AppField
-                                        name="phone"
-                                        validators={{
-                                                onBlur: ({
-                                                        value,
-                                                }: {
-                                                        value: string;
-                                                }) => {
-                                                        if (
-                                                                !value ||
-                                                                value.trim()
-                                                                        .length ===
-                                                                        0
-                                                        ) {
-                                                                return 'Số điện thoại không được để trống';
-                                                        }
-                                                        return undefined;
-                                                },
-                                        }}
-                                >
-                                        {(field: any) => (
-                                                <field.TextField
-                                                        label="Số điện thoại"
-                                                        placeholder="123-456-7890"
-                                                />
-                                        )}
-                                </form.AppField>
-                                <form.AppField name="dob">
-                                        {(field: any) => (
-                                                <field.DatePicker label="Sinh nhật" />
-                                        )}
-                                </form.AppField>
-                        </div>
-                </div>
-        );
+			{/* Phone - Full Width */}
+			<div className='grid grid-cols-2 gap-6'>
+				<form.AppField
+					name='phone'
+					validators={{
+						onBlur: ({ value }: { value: string }) => {
+							if (!value || value.trim().length === 0) {
+								return 'Số điện thoại không được để trống'
+							}
+							return undefined
+						}
+					}}
+				>
+					{(field: any) => (
+						<field.TextField
+							label='Số điện thoại'
+							placeholder='123-456-7890'
+						/>
+					)}
+				</form.AppField>
+				<form.AppField name='dob'>
+					{(field: any) => <field.DatePicker label='Sinh nhật' />}
+				</form.AppField>
+			</div>
+		</div>
+	)
 }
