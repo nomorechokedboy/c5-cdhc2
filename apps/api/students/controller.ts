5f10e1e208972eb426505f3054a5f90964728c69
nomorechokedboy <haiduongit@protonmail.com>
2025-08-07 14:17:35 +0700

    feat(students): get students by unit

--- apps/api/students/controller.ts
+++ apps/api/students/controller.ts
@@ -1,65 +1,122 @@
-import { AppError } from '../errors/index.js';
+import { AppError } from '../errors'
 import {
-        StudentDB,
-        StudentParam,
-        StudentQuery,
-        Student,
-        UpdateStudentMap,
-} from '../schema/student.js';
-import { Repository } from './index.js';
-import studentRepo from './repo.js';
+	StudentDB,
+	StudentParam,
+	Student,
+	UpdateStudentMap
+} from '../schema/student.js'
+import { Repository } from './index'
+import { Repository as UnitRepository } from '../units'
+import studentRepo from './repo.js'
+import { GetStudentsQuery } from './students.js'
+import unitRepo from '../units/repo.js'
+import log from 'encore.dev/log'
 
 export class Controller {
-        constructor(private repo: Repository) {}
+	constructor(
+		private readonly repo: Repository,
+		private readonly unitRepo: UnitRepository
+	) {}
 
-        create(params: StudentParam[]): Promise<StudentDB[]> {
-                return this.repo.create(params).catch(AppError.handleAppErr);
-        }
+	create(params: StudentParam[]): Promise<StudentDB[]> {
+		return this.repo.create(params).catch(AppError.handleAppErr)
+	}
 
-        delete(students: StudentDB[]) {
-                return this.repo.delete(students).catch(AppError.handleAppErr);
-        }
+	delete(students: StudentDB[]) {
+		return this.repo.delete(students).catch(AppError.handleAppErr)
+	}
 
-        find(q: StudentQuery): Promise<Student[]> {
-                return this.repo.find(q).catch(AppError.handleAppErr);
-        }
+	async find({
+		unitAlias,
+		unitLevel,
+		classId,
+		...q
+	}: GetStudentsQuery): Promise<Student[]> {
+		const isUnitAliasExist = unitAlias !== undefined
+		const isUnitLevelExist = unitLevel !== undefined
+		const isUnitQueryParamsValid = isUnitAliasExist && isUnitLevelExist
 
-        async update(params: StudentDB[]): Promise<StudentDB[]> {
-                const ids = params.map((s) => s.id);
-                const isIdsEmpty = ids.length === 0;
-                const isIdsValid = !ids || isIdsEmpty;
-                if (isIdsValid) {
-                        throw AppError.invalidArgument(
-                                'No record IDs provided'
-                        );
-                }
+		if (!isUnitQueryParamsValid) {
+			throw AppError.invalidArgument('missing unitAlias or unitLevel')
+		}
 
-                const updateMap: UpdateStudentMap = params.map(
-                        ({ id, ...updatePayload }) => {
-                                const cleanupPayload = Object.fromEntries(
-                                        Object.entries(updatePayload).filter(
-                                                ([_, value]) =>
-                                                        value !== undefined
-                                        )
-                                );
+		if (isUnitQueryParamsValid) {
+			const u = await this.unitRepo
+				.findOne({ alias: unitAlias, level: unitLevel })
+				.catch(AppError.handleAppErr)
+			if (u === undefined) {
+				throw AppError.handleAppErr(
+					AppError.notFound(
+						`unit with alias: ${unitAlias} and level: ${unitLevel} not found`
+					)
+				)
+			}
 
-                                const isUpdatePayloadEmpty =
-                                        Object.keys(cleanupPayload).length ===
-                                        0;
-                                if (isUpdatePayloadEmpty) {
-                                        throw AppError.invalidArgument(
-                                                `No update data provided At least one field must be provided to update record with id: ${id}`
-                                        );
-                                }
+			if (u.level === 'battalion') {
+				const classIds = u.children
+					.map((c) => c.classes.map((cl) => cl.id))
+					.flat()
+				log.trace('studentRepo.find battalion case classIds', {
+					classIds,
+					query: q
+				})
+				return this.repo
+					.find({ ...q, classIds })
+					.catch(AppError.handleAppErr)
+			}
 
-                                return { id, updatePayload: cleanupPayload };
-                        }
-                );
+			if (u.level === 'company') {
+				const classIds = u.classes.map((c) => c.id)
+				log.trace('studentRepo.find company case classIds', {
+					classIds,
+					query: q
+				})
+				return this.repo
+					.find({ ...q, classIds })
+					.catch(AppError.handleAppErr)
+			}
+		}
 
-                return this.repo.update(updateMap).catch(AppError.handleAppErr);
-        }
+		const classIds = classId ? [classId] : []
+		return this.repo.find({ ...q, classIds }).catch(AppError.handleAppErr)
+	}
+
+	async update(params: StudentDB[]): Promise<StudentDB[]> {
+		const ids = params.map((s) => s.id)
+		const isIdsEmpty = ids.length === 0
+		const isIdsValid = !ids || isIdsEmpty
+		if (isIdsValid) {
+			throw AppError.handleAppErr(
+				AppError.invalidArgument('No record IDs provided')
+			)
+		}
+
+		const updateMap: UpdateStudentMap = params.map(
+			({ id, ...updatePayload }) => {
+				const cleanupPayload = Object.fromEntries(
+					Object.entries(updatePayload).filter(
+						([_, value]) => value !== undefined
+					)
+				)
+
+				const isUpdatePayloadEmpty =
+					Object.keys(cleanupPayload).length === 0
+				if (isUpdatePayloadEmpty) {
+					throw AppError.handleAppErr(
+						AppError.invalidArgument(
+							`No update data provided At least one field must be provided to update record with id: ${id}`
+						)
+					)
+				}
+
+				return { id, updatePayload: cleanupPayload }
+			}
+		)
+
+		return this.repo.update(updateMap).catch(AppError.handleAppErr)
+	}
 }
 
-const studentController = new Controller(studentRepo);
+const studentController = new Controller(studentRepo, unitRepo)
 
-export default studentController;
+export default studentController
