54210bf9921758e19431204a93b92aa32c50efc8
nomorechokedboy <haiduongit@protonmail.com>
2025-08-07 15:43:37 +0700

    feat(web): impl company student tab

--- apps/web/src/routes/dai-doi/$companyAlias.tsx
+++ apps/web/src/routes/dai-doi/$companyAlias.tsx
@@ -1,72 +1,37 @@
-import ClassForm from '@/components/class-form'
-import ClassCard from '@/components/class-table/class-card'
-import { columns } from '@/components/class-table/columns'
-import { DataTable } from '@/components/data-table'
-import useDataTableToolbarConfig from '@/hooks/useDataTableToolbarConfig'
-import useUnitsData from '@/hooks/useUnitsData'
-import type { FacetedFilterConfig } from '@/types'
-import { createFileRoute, redirect } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 import ProtectedRoute from '@/components/ProtectedRoute'
+import CompanyClassesTable from '@/components/company-classes-table'
+import CompanyStudentTable from '@/components/company-student-table'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 export const Route = createFileRoute('/dai-doi/$companyAlias')({
-	
-	component: RouteComponent
+    component: RouteComponent
 })
 
 function RouteComponent() {
-	const { createSearchConfig } = useDataTableToolbarConfig()
-	const { companyAlias } = Route.useParams()
-	const { data: units, refetch: refetchUnits } = useUnitsData({
-		level: 'battalion'
-	})
-	const flatUnits = units
-		?.map((unit) => {
-			if (unit.children.length !== 0) {
-				return [unit, ...unit.children]
-			}
+    const { companyAlias } = Route.useParams()
 
-			return [unit]
-		})
-		.flat()
-	const company = flatUnits?.find((unit) => unit.alias === companyAlias)
-	const handleFormSuccess = () => {
-		refetchUnits()
-	}
+    return (
+        <ProtectedRoute>
+            <div className='hidden h-full flex-1 flex-col space-y-8 p-8 md:flex'>
+                <Tabs defaultValue='classes'>
+                    <TabsList>
+                        <TabsTrigger value='classes'>Lớp</TabsTrigger>
+                        <TabsTrigger value='students'>Học viên</TabsTrigger>
+                    </TabsList>
 
-	const searchConfig = [
-		createSearchConfig('name', 'Tìm kiếm theo tên lớp...')
-	]
-	const facetedFilters: FacetedFilterConfig[] = []
+                    <TabsContent value='classes'>
+                        <CompanyClassesTable companyAlias={companyAlias} />
+                    </TabsContent>
 
-	return (
-		<ProtectedRoute>
-			<div className='hidden h-full flex-1 flex-col space-y-8 p-8 md:flex'>
-				<div className='flex items-center justify-between space-y-2'>
-					<div>
-						<h2 className='text-2xl font-bold tracking-tight'>
-							Danh sách lớp của {company?.name}
-						</h2>
-					</div>
-				</div>
-				<DataTable
-					placeholder='Đại đội chưa có lớp nào'
-					columns={columns}
-					cardComponent={ClassCard}
-					cardClassName='grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'
-					data={company?.classes ?? []}
-					defaultViewMode='card'
-					toolbarProps={{
-						rightSection: (
-							<ClassForm
-								onSuccess={handleFormSuccess}
-								unitId={company?.id}
-							/>
-						),
-						searchConfig,
-						facetedFilters
-					}}
-				/>
-			</div>
-		</ProtectedRoute>
-	)
+                    <TabsContent value='students'>
+                        <CompanyStudentTable
+                            alias={companyAlias}
+                            level='company'
+                        />
+                    </TabsContent>
+                </Tabs>
+            </div>
+        </ProtectedRoute>
+    )
 }
