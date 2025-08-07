10c2a6168f243cfd50f79a31a975a90b9f97a35b
nomorechokedboy <haiduongit@protonmail.com>
2025-08-07 14:16:24 +0700

    feat(unit): get unit by alias and level

--- apps/api/units/index.ts
+++ apps/api/units/index.ts
@@ -4,5 +4,8 @@ export interface Repository {
 	create(params: UnitParams[]): Promise<UnitDB[]>
 	delete(units: UnitDB[]): Promise<UnitDB[]>
 	find(query: UnitQuery): Promise<Unit[]>
-	findOne(params: { id: number }): Promise<UnitDB | undefined>
+	findOne(params: {
+		alias: string
+		level: 'battalion' | 'company'
+	}): Promise<Unit | undefined>
 }
