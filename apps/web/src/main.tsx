2701b7d0f4ccb15e94b3a391e1cfe2a21aa575dc
nomorechokedboy <haiduongit@protonmail.com>
2025-08-08 21:11:33 +0700

    feat(web): add label to column metadata
    add label field to tanstack table column def metadata

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 
 import * as TanStackQueryProvider from './integrations/tanstack-query/root-provider.tsx'
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
@@ -13,36 +13,43 @@ import reportWebVitals from './reportWebVitals.ts'
 
 // Create a new router instance
 const router = createRouter({
-  routeTree,
-  context: {
-    ...TanStackQueryProvider.getContext(),
-  },
-  defaultPreload: 'intent',
-  scrollRestoration: true,
-  defaultStructuralSharing: true,
-  defaultPreloadStaleTime: 0,
+	routeTree,
+	context: {
+		...TanStackQueryProvider.getContext()
+	},
+	defaultPreload: 'intent',
+	scrollRestoration: true,
+	defaultStructuralSharing: true,
+	defaultPreloadStaleTime: 0
 })
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
+	interface Register {
+		router: typeof router
+	}
+}
+
+declare module '@tanstack/react-table' {
+	interface ColumnMeta<TData, TValue> {
+		label?: string
+		// Add any other custom meta properties you use
+	}
 }
 
 // Render the app
 const rootElement = document.getElementById('app')
 if (rootElement && !rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(
-    <StrictMode>
-      <AuthProvider>
-        <TanStackQueryProvider.Provider>
-          <RouterProvider router={router} />
-        </TanStackQueryProvider.Provider>
-      </AuthProvider>
-    </StrictMode>
-  )
+	const root = ReactDOM.createRoot(rootElement)
+	root.render(
+		<StrictMode>
+			<AuthProvider>
+				<TanStackQueryProvider.Provider>
+					<RouterProvider router={router} />
+				</TanStackQueryProvider.Provider>
+			</AuthProvider>
+		</StrictMode>
+	)
 }
 
 // If you want to start measuring performance in your app, pass a function
