bd6683b6d11c1c53e370d0bbf98543ed176a7c12
nomorechokedboy <haiduongit@protonmail.com>
2025-08-11 09:04:01 +0700

    feat(web): open student details from row action

--- apps/web/src/components/data-table/data-table-row-actions.tsx
+++ apps/web/src/components/data-table/data-table-row-actions.tsx
@@ -1,82 +1,70 @@
-import type { Row } from '@tanstack/react-table';
-import { MoreHorizontal } from 'lucide-react';
-import { Button } from '@/components/ui/button';
+import type { Row } from '@tanstack/react-table'
+import { MoreHorizontal } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 import {
-        DropdownMenu,
-        DropdownMenuContent,
-        DropdownMenuItem,
-        DropdownMenuRadioGroup,
-        DropdownMenuRadioItem,
-        DropdownMenuSeparator,
-        DropdownMenuShortcut,
-        DropdownMenuSub,
-        DropdownMenuSubContent,
-        DropdownMenuSubTrigger,
-        DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { labels } from './data/data';
-import { taskSchema } from './data/schema';
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuShortcut,
+	DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import type { Student } from '@/types'
+import {
+	Dialog,
+	DialogHeader,
+	DialogTitle,
+	DialogContent
+} from '@/components/ui/dialog'
+import StudentInfoTabs from '../student-info-tabs'
+import { useState } from 'react'
 
 interface DataTableRowActionsProps<TData> {
-        row: Row<TData>;
+	row: Row<TData>
 }
 
 export function DataTableRowActions<TData>({
-        row,
+	row
 }: DataTableRowActionsProps<TData>) {
-        // const task = taskSchema.parse(row.original);
+	const student = row.original as unknown as Student
+	const [dialogOpen, setDialogOpen] = useState(false)
 
-        return (
-                <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                                <Button
-                                        variant="ghost"
-                                        className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-                                >
-                                        <MoreHorizontal />
-                                        <span className="sr-only">
-                                                Open menu
-                                        </span>
-                                </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end" className="w-[160px]">
-                                <DropdownMenuItem>Edit</DropdownMenuItem>
-                                <DropdownMenuItem>Make a copy</DropdownMenuItem>
-                                <DropdownMenuItem>Favorite</DropdownMenuItem>
-                                <DropdownMenuSeparator />
-                                <DropdownMenuSub>
-                                        <DropdownMenuSubTrigger>
-                                                Labels
-                                        </DropdownMenuSubTrigger>
-                                        {/* <DropdownMenuSubContent>
-                                                <DropdownMenuRadioGroup
-                                                        value={task.label}
-                                                >
-                                                        {labels.map((label) => (
-                                                                <DropdownMenuRadioItem
-                                                                        key={
-                                                                                label.value
-                                                                        }
-                                                                        value={
-                                                                                label.value
-                                                                        }
-                                                                >
-                                                                        {
-                                                                                label.label
-                                                                        }
-                                                                </DropdownMenuRadioItem>
-                                                        ))}
-                                                </DropdownMenuRadioGroup>
-                                        </DropdownMenuSubContent> */}
-                                </DropdownMenuSub>
-                                <DropdownMenuSeparator />
-                                <DropdownMenuItem>
-                                        Delete
-                                        <DropdownMenuShortcut>
-                                                ⌘⌫
-                                        </DropdownMenuShortcut>
-                                </DropdownMenuItem>
-                        </DropdownMenuContent>
-                </DropdownMenu>
-        );
+	function handleOpenDialog() {
+		setDialogOpen(true)
+	}
+
+	return (
+		<>
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Button
+						variant='ghost'
+						className='flex h-8 w-8 p-0 data-[state=open]:bg-muted'
+					>
+						<MoreHorizontal />
+						<span className='sr-only'>Open menu</span>
+					</Button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent align='end' className='w-[160px]'>
+					<DropdownMenuItem onClick={handleOpenDialog}>
+						Chi tiết
+					</DropdownMenuItem>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>
+						Xóa
+						<DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+			<Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+				<DialogContent className='max-w-7xl h-[90vh] overflow-y-auto p-6'>
+					<DialogHeader className='flex items-center justify-between'>
+						<DialogTitle>Thông tin học viên</DialogTitle>
+					</DialogHeader>
+
+					<StudentInfoTabs student={student} />
+				</DialogContent>
+			</Dialog>
+		</>
+	)
 }
