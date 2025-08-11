7cc6713b735b36b55549a6b00c0d230dae336ca0
nomorechokedboy <haiduongit@protonmail.com>
2025-08-11 20:27:50 +0700

    feat(web): impl search in column filter header

--- apps/web/src/components/data-table/data-table-column-header.tsx
+++ apps/web/src/components/data-table/data-table-column-header.tsx
@@ -1,86 +1,100 @@
-import type { Column } from '@tanstack/react-table';
-import { ArrowDown, ArrowUp, ChevronsUpDown, EyeOff } from 'lucide-react';
-
-import { cn } from '@/lib/utils';
-import { Button } from '@/components/ui/button';
+import type { Column } from '@tanstack/react-table'
+import { ArrowDown, ArrowUp, ChevronsUpDown, EyeOff, XIcon } from 'lucide-react'
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
 import {
-        DropdownMenu,
-        DropdownMenuContent,
-        DropdownMenuItem,
-        DropdownMenuSeparator,
-        DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { Input } from '@/components/ui/input'
+import { useState, type ChangeEvent } from 'react'
+import { useDebounce } from 'react-use'
 
 interface DataTableColumnHeaderProps<TData, TValue>
-        extends React.HTMLAttributes<HTMLDivElement> {
-        column: Column<TData, TValue>;
-        title: string;
+	extends React.HTMLAttributes<HTMLDivElement> {
+	column: Column<TData, TValue>
+	title: string
 }
 
 export function DataTableColumnHeader<TData, TValue>({
-        column,
-        title,
-        className,
+	column,
+	title,
+	className
 }: DataTableColumnHeaderProps<TData, TValue>) {
-        if (!column.getCanSort()) {
-                return <div className={cn(className)}>{title}</div>;
-        }
+	if (!column.getCanSort()) {
+		return <div className={cn(className)}>{title}</div>
+	}
 
-        return (
-                <div className={cn('flex items-center space-x-2', className)}>
-                        <DropdownMenu>
-                                <DropdownMenuTrigger asChild>
-                                        <Button
-                                                variant="ghost"
-                                                size="sm"
-                                                className="-ml-3 h-8 data-[state=open]:bg-accent"
-                                        >
-                                                <span>{title}</span>
-                                                {column.getIsSorted() ===
-                                                'desc' ? (
-                                                        <ArrowDown />
-                                                ) : column.getIsSorted() ===
-                                                  'asc' ? (
-                                                        <ArrowUp />
-                                                ) : (
-                                                        <ChevronsUpDown />
-                                                )}
-                                        </Button>
-                                </DropdownMenuTrigger>
-                                <DropdownMenuContent align="start">
-                                        <DropdownMenuItem
-                                                onClick={() =>
-                                                        column.toggleSorting(
-                                                                false
-                                                        )
-                                                }
-                                        >
-                                                <ArrowUp className="h-3.5 w-3.5 text-muted-foreground/70" />
-                                                Từ A-Z
-                                        </DropdownMenuItem>
-                                        <DropdownMenuItem
-                                                onClick={() =>
-                                                        column.toggleSorting(
-                                                                true
-                                                        )
-                                                }
-                                        >
-                                                <ArrowDown className="h-3.5 w-3.5 text-muted-foreground/70" />
-                                                Từ Z-A
-                                        </DropdownMenuItem>
-                                        <DropdownMenuSeparator />
-                                        <DropdownMenuItem
-                                                onClick={() =>
-                                                        column.toggleVisibility(
-                                                                false
-                                                        )
-                                                }
-                                        >
-                                                <EyeOff className="h-3.5 w-3.5 text-muted-foreground/70" />
-                                                Hide
-                                        </DropdownMenuItem>
-                                </DropdownMenuContent>
-                        </DropdownMenu>
-                </div>
-        );
+	const [searchValue, setSearchValue] = useState(() => {
+		return (column.getFilterValue() as string) ?? ''
+	})
+
+	const [, cancel] = useDebounce(
+		() => {
+			column.setFilterValue(searchValue || undefined)
+		},
+		300, // 300ms delay
+		[searchValue]
+	)
+
+	if (!column.getCanSort()) {
+		return <div className={cn(className)}>{title}</div>
+	}
+
+	function handleSearchChange(event: ChangeEvent<HTMLInputElement>) {
+		setSearchValue(event.target.value)
+	}
+
+	return (
+		<div className={cn('flex items-center space-x-2', className)}>
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Button
+						variant='ghost'
+						size='sm'
+						className='-ml-3 h-8 data-[state=open]:bg-accent'
+					>
+						<span>{title}</span>
+						{column.getIsSorted() === 'desc' ? (
+							<ArrowDown />
+						) : column.getIsSorted() === 'asc' ? (
+							<ArrowUp />
+						) : (
+							<ChevronsUpDown />
+						)}
+					</Button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent align='start'>
+					<Input
+						placeholder='Tìm kiếm...'
+						value={searchValue}
+						onChange={handleSearchChange}
+						type='search'
+					/>
+					<DropdownMenuItem
+						onClick={() => column.toggleSorting(false)}
+					>
+						<ArrowUp className='h-3.5 w-3.5 text-muted-foreground/70' />
+						Từ A-Z
+					</DropdownMenuItem>
+					<DropdownMenuItem
+						onClick={() => column.toggleSorting(true)}
+					>
+						<ArrowDown className='h-3.5 w-3.5 text-muted-foreground/70' />
+						Từ Z-A
+					</DropdownMenuItem>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem
+						onClick={() => column.toggleVisibility(false)}
+					>
+						<EyeOff className='h-3.5 w-3.5 text-muted-foreground/70' />
+						Ẩn cột
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</div>
+	)
 }
