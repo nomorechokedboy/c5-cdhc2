2b17812a4c6d273a0784598f85056d7016e12ad4
nomorechokedboy <haiduongit@protonmail.com>
2025-08-11 09:05:29 +0700

    feat(web): impl new utils using dayjs

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -3,14 +3,26 @@ import { twMerge } from 'tailwind-merge'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import 'dayjs/locale/vi'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+import quarterOfYear from 'dayjs/plugin/quarterOfYear'
 
 dayjs.locale('vi')
 dayjs.extend(relativeTime)
+dayjs.extend(weekOfYear)
+dayjs.extend(quarterOfYear)
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
 
-export const formatTimestamp = (timestamp: string) => {
+export function formatTimestamp(timestamp: string) {
 	return dayjs(timestamp).fromNow()
+}
+
+export function getCurrentWeekNumber() {
+	return dayjs().week()
+}
+
+export function getCurrentQuarter() {
+	return dayjs().quarter()
 }
