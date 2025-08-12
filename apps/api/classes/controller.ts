2c2f7de904a312718bbf016870fcc071eacfd4d5
nomorechokedboy <haiduongit@protonmail.com>
2025-08-12 19:32:40 +0700

    feat(classes): impl update, delete classes

--- apps/api/classes/controller.ts
+++ apps/api/classes/controller.ts
@@ -1,20 +1,87 @@
 import { AppError } from '../errors/index'
-import { ClassDB, ClassParam } from '../schema/classes'
+import { ClassDB, ClassParam, UpdateClassMap } from '../schema/classes'
 import { Repository } from './index'
+import { Repository as UnitRepository } from '../units'
 import sqliteRepo from './repo'
+import log from 'encore.dev/log'
+import unitRepo from '../units/repo'
 
 class Controller {
-	constructor(private readonly repo: Repository) {}
+	constructor(
+		private readonly repo: Repository,
+		private readonly unitRepo: UnitRepository
+	) {}
 
-	create(classParam: ClassParam[]): Promise<ClassDB[]> {
-		return this.repo.create(classParam).catch(AppError.handleAppErr)
+	async create(classParams: ClassParam[]): Promise<ClassDB[]> {
+		log.trace('classController.create params', { classParam: classParams })
+
+		const unitIds = classParams.map((p) => p.unitId)
+		const units = await this.unitRepo.findByIds(unitIds)
+		const isNotfoundUnit = units.length !== unitIds.length
+		if (isNotfoundUnit) {
+			throw AppError.handleAppErr(
+				AppError.invalidArgument('There are invalid unit ids')
+			)
+		}
+
+		const isBattalionUnitExist = units.some(
+			(unit) => unit.level === 'battalion'
+		)
+		if (isBattalionUnitExist) {
+			throw AppError.handleAppErr(
+				AppError.invalidArgument(
+					'There are invalid unit. Class unit must be company'
+				)
+			)
+		}
+
+		return this.repo.create(classParams).catch(AppError.handleAppErr)
+	}
+
+	delete(classes: ClassDB[]) {
+		return this.repo.delete(classes).catch(AppError.handleAppErr)
 	}
 
 	find(): Promise<ClassDB[]> {
 		return this.repo.find({}).catch(AppError.handleAppErr)
 	}
+
+	async update(params: ClassDB[]): Promise<ClassDB[]> {
+		const ids = params.map((s) => s.id)
+		const isIdsEmpty = ids.length === 0
+		const isIdsValid = !ids || isIdsEmpty
+		if (isIdsValid) {
+			throw AppError.handleAppErr(
+				AppError.invalidArgument('No record IDs provided')
+			)
+		}
+
+		const updateMap: UpdateClassMap = params.map(
+			({ id, ...updatePayload }) => {
+				const cleanupPayload = Object.fromEntries(
+					Object.entries(updatePayload).filter(
+						([_, value]) => value !== undefined
+					)
+				)
+
+				const isUpdatePayloadEmpty =
+					Object.keys(cleanupPayload).length === 0
+				if (isUpdatePayloadEmpty) {
+					throw AppError.handleAppErr(
+						AppError.invalidArgument(
+							`No update data provided At least one field must be provided to update record with id: ${id}`
+						)
+					)
+				}
+
+				return { id, updatePayload: cleanupPayload }
+			}
+		)
+
+		return this.repo.update(updateMap).catch(AppError.handleAppErr)
+	}
 }
 
-const classController = new Controller(sqliteRepo)
+const classController = new Controller(sqliteRepo, unitRepo)
 
 export default classController
--- apps/api/classes/index.ts
+++ apps/api/classes/index.ts
@@ -1,9 +1,15 @@
-import { Class, ClassDB, ClassParam, ClassQuery } from '../schema/classes.js'
+import {
+	Class,
+	ClassDB,
+	ClassParam,
+	ClassQuery,
+	UpdateClassMap
+} from '../schema/classes.js'
 
 export interface Repository {
 	create(params: ClassParam[]): Promise<ClassDB[]>
 	delete(classes: ClassDB[]): Promise<ClassDB[]>
 	find(q: ClassQuery): Promise<Class[]>
 	findOne(c: ClassDB): Promise<Class>
-	update(params: ClassDB[]): Promise<ClassDB[]>
+	update(params: UpdateClassMap): Promise<ClassDB[]>
 }
