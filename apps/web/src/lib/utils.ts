102255332b1c128f5784e28727e7741e96d0dea3
nomorechokedboy <haiduongit@protonmail.com>
2025-08-16 07:32:37 +0700

    feat(web): add toVNTz util fn

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -5,11 +5,17 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 import 'dayjs/locale/vi'
 import weekOfYear from 'dayjs/plugin/weekOfYear'
 import quarterOfYear from 'dayjs/plugin/quarterOfYear'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
 
 dayjs.locale('vi')
 dayjs.extend(relativeTime)
 dayjs.extend(weekOfYear)
 dayjs.extend(quarterOfYear)
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
+dayjs.tz.setDefault('Asia/Ho_Chi_Minh')
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
@@ -25,4 +31,8 @@ export function getCurrentWeekNumber() {
 
 export function getCurrentQuarter() {
 	return dayjs().quarter()
+}
+
+export function toVNTz(utcTimestamp: string) {
+	return dayjs.utc(utcTimestamp).format('DD-MM-YYYY')
 }
