cd1079d06116f850b5175bf473af23984cde9455
nomorechokedboy <haiduongit@protonmail.com>
2025-08-16 07:32:37 +0700

    feat(web): add toVNTz util fn

--- apps/web/src/components/class-table/class-card.tsx
+++ apps/web/src/components/class-table/class-card.tsx
@@ -1,42 +1,44 @@
 import {
-        Card,
-        CardHeader,
-        CardTitle,
-        CardFooter,
-        CardDescription,
-} from '@/components/ui/card';
-import type { Class } from '@/types';
-import { EllipsisText } from '@/components/data-table/ellipsis-text';
-import { Link } from '@tanstack/react-router';
+	Card,
+	CardHeader,
+	CardTitle,
+	CardFooter,
+	CardDescription
+} from '@/components/ui/card'
+import type { Class } from '@/types'
+import { EllipsisText } from '@/components/data-table/ellipsis-text'
+import { Link } from '@tanstack/react-router'
+import { toVNTz } from '@/lib/utils'
 
 interface ClassCardProps {
-        data: Class;
-        index: number;
+	data: Class
+	index: number
 }
 
 export default function ClassCard({ data, index }: ClassCardProps) {
-        return (
-                <Link to={`/classes/$classId`} params={(prev) => ({ ...prev, classId: data.id })}>
-                <Card className="@container/card">
-                        <CardHeader>
-                                <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-                                        {data.name}
-                                </CardTitle>
-                                <CardDescription>
-                                        <EllipsisText>
-                                                {data.description}
-                                        </EllipsisText>
-                                </CardDescription>
-                        </CardHeader>
-                        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-                                <div className="line-clamp-1 flex gap-2 font-medium">
-                                        Tổng số học viên: {data.studentCount}
-                                </div>
-                                <div className="text-muted-foreground">
-                                        Tạo ngày: {data.createdAt}
-                                </div>
-                        </CardFooter>
-                </Card>
-                </Link>
-        );
+	return (
+		<Link
+			to={`/classes/$classId`}
+			params={(prev) => ({ ...prev, classId: data.id })}
+		>
+			<Card className='@container/card'>
+				<CardHeader>
+					<CardTitle className='text-2xl font-semibold tabular-nums @[250px]/card:text-3xl'>
+						{data.name}
+					</CardTitle>
+					<CardDescription>
+						<EllipsisText>{data.description}</EllipsisText>
+					</CardDescription>
+				</CardHeader>
+				<CardFooter className='flex-col items-start gap-1.5 text-sm'>
+					<div className='line-clamp-1 flex gap-2 font-medium'>
+						Tổng số học viên: {data.studentCount}
+					</div>
+					<div className='text-muted-foreground'>
+						Tạo ngày: {toVNTz(data.createdAt)}
+					</div>
+				</CardFooter>
+			</Card>
+		</Link>
+	)
 }
