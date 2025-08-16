772556eb276a7803cde46af4347391008bc233b0
nomorechokedboy <haiduongit@protonmail.com>
2025-08-16 07:33:41 +0700

    feat(web): add combobox form component

--- apps/web/src/hooks/demo.form.ts
+++ apps/web/src/hooks/demo.form.ts
@@ -1,24 +1,26 @@
-import DatePicker from '@/components/date-picker';
-import { createFormHook } from '@tanstack/react-form';
+import DatePicker from '@/components/date-picker'
+import { createFormHook } from '@tanstack/react-form'
 
 import {
-        Select,
-        SubscribeButton,
-        TextArea,
-        TextField,
-} from '../components/demo.FormComponents';
-import { fieldContext, formContext } from './demo.form-context';
+	Select,
+	SubscribeButton,
+	TextArea,
+	TextField,
+	Combobox
+} from '../components/demo.FormComponents'
+import { fieldContext, formContext } from './demo.form-context'
 
 export const { useAppForm } = createFormHook({
-        fieldComponents: {
-                TextField,
-                Select,
-                TextArea,
-                DatePicker,
-        },
-        formComponents: {
-                SubscribeButton,
-        },
-        fieldContext,
-        formContext,
-});
+	fieldComponents: {
+		TextField,
+		Select,
+		TextArea,
+		DatePicker,
+		Combobox
+	},
+	formComponents: {
+		SubscribeButton
+	},
+	fieldContext,
+	formContext
+})
