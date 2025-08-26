e8a286e2d53a0e071e03cef7b780595166706dca
nomorechokedboy <haiduongit@protonmail.com>
2025-08-26 09:45:20 +0700

    feat(web): impl useUnitData

---
+++ apps/web/src/hooks/useUnitData.ts
@@ -0,0 +1,12 @@
+import { GetUnit } from '@/api'
+import type { units } from '@/api/client'
+import { useQuery } from '@tanstack/react-query'
+
+export default function useUnitData(
+	params: units.GetUnitRequest & { alias: string }
+) {
+	return useQuery({
+		queryKey: ['unit'],
+		queryFn: () => GetUnit(params)
+	})
+}
