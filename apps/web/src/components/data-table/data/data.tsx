a5a851482be02edd86c63162c1d14cb6b3258430
nomorechokedboy <haiduongit@protonmail.com>
2025-08-27 16:16:09 +0700

    feat(web): impl editable military rank combobox

--- apps/web/src/components/data-table/data/data.tsx
+++ apps/web/src/components/data-table/data/data.tsx
@@ -1,95 +1,113 @@
 import {
-        ArrowDown,
-        ArrowRight,
-        ArrowUp,
-        CheckCircle,
-        Circle,
-        CircleOff,
-        HelpCircle,
-        Timer,
-} from 'lucide-react';
+	ArrowDown,
+	ArrowRight,
+	ArrowUp,
+	CheckCircle,
+	Circle,
+	CircleOff,
+	HelpCircle,
+	Timer
+} from 'lucide-react'
 
 export const labels = [
-        {
-                value: 'bug',
-                label: 'Bug',
-        },
-        {
-                value: 'feature',
-                label: 'Feature',
-        },
-        {
-                value: 'documentation',
-                label: 'Documentation',
-        },
-];
+	{
+		value: 'bug',
+		label: 'Bug'
+	},
+	{
+		value: 'feature',
+		label: 'Feature'
+	},
+	{
+		value: 'documentation',
+		label: 'Documentation'
+	}
+]
 
 export const statuses = [
-        {
-                value: 'backlog',
-                label: 'Backlog',
-                icon: HelpCircle,
-        },
-        {
-                value: 'todo',
-                label: 'Todo',
-                icon: Circle,
-        },
-        {
-                value: 'in progress',
-                label: 'In Progress',
-                icon: Timer,
-        },
-        {
-                value: 'done',
-                label: 'Done',
-                icon: CheckCircle,
-        },
-        {
-                value: 'canceled',
-                label: 'Canceled',
-                icon: CircleOff,
-        },
-];
+	{
+		value: 'backlog',
+		label: 'Backlog',
+		icon: HelpCircle
+	},
+	{
+		value: 'todo',
+		label: 'Todo',
+		icon: Circle
+	},
+	{
+		value: 'in progress',
+		label: 'In Progress',
+		icon: Timer
+	},
+	{
+		value: 'done',
+		label: 'Done',
+		icon: CheckCircle
+	},
+	{
+		value: 'canceled',
+		label: 'Canceled',
+		icon: CircleOff
+	}
+]
 
 export const priorities = [
-        {
-                label: 'Low',
-                value: 'low',
-                icon: ArrowDown,
-        },
-        {
-                label: 'Medium',
-                value: 'medium',
-                icon: ArrowRight,
-        },
-        {
-                label: 'High',
-                value: 'high',
-                icon: ArrowUp,
-        },
-];
+	{
+		label: 'Low',
+		value: 'low',
+		icon: ArrowDown
+	},
+	{
+		label: 'Medium',
+		value: 'medium',
+		icon: ArrowRight
+	},
+	{
+		label: 'High',
+		value: 'high',
+		icon: ArrowUp
+	}
+]
 
 export const MilitaryRankOptions = [
-        'Thiếu úy chuyên nghiệp',
-        'Trung sỹ',
-        'Binh nhất',
-].map((el) => ({ label: el, value: el }));
+	'Binh nhì',
+	'Binh nhất',
+	'Hạ sĩ',
+	'Trung sĩ',
+	'Thượng sĩ',
+	'Thiếu úy',
+	'Trung úy',
+	'Thượng úy',
+	'Đại úy',
+	'Thiếu tá',
+	'Trung tá',
+	'Thượng tá',
+	'Đại tá',
+	'Thiếu úy chuyên nghiệp',
+	'Trung úy chuyên nghiệp',
+	'Thượng úy chuyên nghiệp',
+	'Đại úy chuyên nghiệp',
+	'Thiếu tá chuyên nghiệp',
+	'Trung tá chuyên nghiệp',
+	'Thượng tá chuyên nghiệp',
+	'Đại tá chuyên nghiệp'
+].map((el) => ({ label: el, value: el }))
 
 export const EduLevelOptions = [
-        '7/12',
-        '8/12',
-        '9/12',
-        '10/12',
-        '11/12',
-        '12/12',
-        'Trung cấp',
-        'Cao đẳng',
-        'Đại học',
-        'Sau đại học',
-].map((el) => ({ label: el, value: el }));
+	'7/12',
+	'8/12',
+	'9/12',
+	'10/12',
+	'11/12',
+	'12/12',
+	'Trung cấp',
+	'Cao đẳng',
+	'Đại học',
+	'Sau đại học'
+].map((el) => ({ label: el, value: el }))
 
 export const ClassOptions = ['K1', 'KCL1'].map((el) => ({
-        label: el,
-        value: el,
-}));
+	label: el,
+	value: el
+}))
