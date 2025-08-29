694f5eaff29dadc9cfe617892f3b8f09fa748103
nomorechokedboy <haiduongit@protonmail.com>
2025-08-29 21:56:17 +0700

    feat(web): enable swc

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,24 +1,23 @@
 import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import reactSWC from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
-    tailwindcss(),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
+	plugins: [
+		TanStackRouterVite({ autoCodeSplitting: true }),
+		tailwindcss(),
+		reactSWC()
+	],
+	test: {
+		globals: true,
+		environment: 'jsdom'
+	},
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, './src')
+		}
+	}
 })
