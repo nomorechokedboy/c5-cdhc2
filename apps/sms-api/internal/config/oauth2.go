dfa960ab47e6f8ef33874eab3bc17404d69c1c46
nomorechokedboy <haiduongit@protonmail.com>
2025-09-08 16:19:15 +0700

    feat(sms-api): impl db config

--- apps/sms-api/internal/config/oauth2.go
+++ apps/sms-api/internal/config/oauth2.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"log/slog"
 )
 
@@ -9,12 +8,18 @@ type Oauth2Config struct {
 	ClientId     string `env:"CLIENT_ID"     json:"client_id"`
 	ClientSecret string `env:"CLIENT_SECRET" json:"client_secret"`
 	OriginUrl    string `env:"ORIGIN_URL"    json:"origin_url"`
+	AuthUrl      string `env:"AUTH_URL"      json:"auth_url"`
+	TokenUrl     string `env:"TOKEN_URL"     json:"token_url"`
 }
 
 var _ slog.LogValuer = (*Oauth2Config)(nil)
 
 func (c *Oauth2Config) LogValue() slog.Value {
 	return slog.GroupValue(
-		slog.String("db_url", fmt.Sprintf("**%s**", c.ClientSecret)),
+		slog.String("client_id", c.ClientId),
+		slog.String("client_secret", generateMaskedString(c.ClientSecret)),
+		slog.String("origin_url", c.OriginUrl),
+		slog.String("auth_url", c.AuthUrl),
+		slog.String("token_url", c.TokenUrl),
 	)
 }
