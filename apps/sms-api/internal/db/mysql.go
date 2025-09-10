fb805ad6098d1c7cb11502231441055e736c860e
nomorechokedboy <haiduongit@protonmail.com>
2025-09-10 19:45:15 +0700

    chore(sms-api): remove debug log

--- apps/sms-api/internal/db/mysql.go
+++ apps/sms-api/internal/db/mysql.go
@@ -4,13 +4,11 @@ import (
 	"log"
 
 	"encore.app/internal/config"
-	"encore.app/internal/logger"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/pocketbase/dbx"
 )
 
 func New(cfg *config.DatabaseConfig) (*dbx.DB, error) {
-	logger.Debug("Debug db dns", "DNS", cfg.GetDNS())
 	db, err := dbx.MustOpen("mysql", cfg.GetDNS())
 	if err != nil {
 		return nil, err
