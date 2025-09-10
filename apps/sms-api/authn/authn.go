fafbdd5969af2eb78e2915ecc60a703e0d4ea9c8
nomorechokedboy <haiduongit@protonmail.com>
2025-09-10 16:16:34 +0700

    chore(sms-api): remove unused code

--- apps/sms-api/authn/authn.go
+++ apps/sms-api/authn/authn.go
@@ -2,38 +2,13 @@ package authn
 
 import (
 	"context"
-	"net/http"
 
 	"encore.app/internal/entities"
 )
 
-// Oauth2 login endpoint
-//
-//encore:api public raw method=GET path=/login
-func OAuth2Login(w http.ResponseWriter, req *http.Request) {
-	container.GetController().HandleLogin(w, req)
-}
-
-type Response struct {
-	Message string
-}
-
 type OAuth2CallbackRequest struct {
 	State string `json:"state" query:"state"`
 	Code  string `json:"code"  query:"code"`
-}
-
-type UserInfo struct {
-	Id          int64  `json:"id"`
-	Address     string `json:"address"`
-	Description string `json:"description"`
-	Email       string `json:"email"`
-	Firstname   string `json:"firstname"`
-	Idnumber    string `json:"idnumber"`
-	Lang        string `json:"lang"`
-	Lastname    string `json:"lastname"`
-	Phone1      string `json:"phone1"`
-	Username    string `json:"username"`
 }
 
 // Oauth2 callback endpoint
