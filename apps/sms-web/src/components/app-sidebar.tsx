96c52b9ce4934450e9d9f7bcb00abad7b89ff1d9
nomorechokedboy <haiduongit@protonmail.com>
2025-09-24 15:37:55 +0700

    feat(sms-web): open sidebar group by default

--- apps/sms-web/src/components/app-sidebar.tsx
+++ apps/sms-web/src/components/app-sidebar.tsx
@@ -1,13 +1,4 @@
-import {
-	UserPlus,
-	Calendar,
-	PieChart,
-	Star,
-	Church,
-	UserCheck,
-	Home,
-	Proportions
-} from 'lucide-react'
+import { Home } from 'lucide-react'
 import { Link } from '@tanstack/react-router'
 import Cdhc2Logo from '@/assets/cdhc2.png'
 import { AppSidebarSkeleton } from '@repo/ui/components/app-sidebar-skeleton'
@@ -15,92 +6,53 @@ import type {
 	NavItem,
 	SidebarConfig,
 	SidebarData,
-	SidebarRenderProps
+	SidebarRenderProps,
+	DataTransformer
 } from '@repo/ui/components/app-sidebar/index'
 import {
 	useSidebarLogic,
 	AppSidebar as GenericSidebar
 } from '@repo/ui/components/app-sidebar/index'
 import { useSidebar, Sidebar } from '@repo/ui/components/ui/sidebar'
+import { useQuery } from '@tanstack/react-query'
+import { CategoryApi } from '@/api'
+import { UsersRound } from 'lucide-react'
+import type { CourseCategory } from '@/types'
+
+class CourseCategoryToNavTransformer
+	implements DataTransformer<CourseCategory, NavItem[]>
+{
+	transform(data: CourseCategory[]): NavItem[] {
+		return data.map((category) => ({
+			title: category.name,
+			url: `/khoa-hoc/${category.idnumber}`,
+			icon: UsersRound,
+			metadata: { category: { id: category.id } }
+		}))
+	}
+}
 
 const APP_BASE_NAVIGATION: SidebarData = {
-	versions: ['1.0.1', '1.1.0-alpha', '2.0.0-beta1'],
 	navMain: [
 		{
 			title: 'Chung',
 			url: '#',
-			items: [
-				{ title: 'Trang chủ', url: '/', icon: Home },
-				{
-					title: 'Chất lượng chính trị',
-					url: '/thong-ke-chinh-tri',
-					icon: Proportions
-				}
-			]
-		},
-		{
-			title: 'Thống kê học viên',
-			url: '#',
-			icon: PieChart,
-			items: [
-				{
-					title: 'Đảng viên',
-					url: '/cpv',
-					icon: UserCheck
-				},
-				{
-					title: 'Đoàn viên',
-					url: '/hcyu',
-					icon: UserPlus
-				},
-				{
-					title: 'Dân tộc thiểu số',
-					url: '/ethnic-minority',
-					icon: Star
-				},
-				{
-					title: 'Tôn giáo',
-					url: '/religion',
-					icon: Church
-				}
-			]
-		},
-		{
-			title: 'Sự kiện học viên',
-			url: '#',
-			icon: Calendar,
-			items: [
-				{
-					title: 'Sinh nhật đồng đội',
-					url: '/birthday',
-					icon: Calendar
-				},
-				{
-					title: '☭ Chuyển Đảng chính thức ',
-					url: '/chuyen-dang-chinh-thuc'
-				}
-			]
-		},
-		{
-			title: 'Chức năng khác',
-			url: '#',
-			icon: Star,
-			items: [
-				{
-					title: 'Import học viên',
-					url: '/import-students',
-					icon: UserPlus
-				}
-			]
+			items: [{ title: 'Trang chủ', url: '/', icon: Home }]
 		}
 	]
 }
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+	const { data: courseCategories = [], isLoading: isCourseCategoryLoading } =
+		useQuery({ queryKey: ['courses'], queryFn: CategoryApi.GetCategories })
+
+	const courseCategoryTransformer = new CourseCategoryToNavTransformer()
 	const { navigationData } = useSidebarLogic({
 		baseNavigation: APP_BASE_NAVIGATION,
 		insertPosition: 1,
-		groupTitle: 'Đơn vị'
+		groupTitle: 'Danh sách lớp',
+		dataTransformer: courseCategoryTransformer,
+		dynamicData: courseCategories
 	})
 
 	const config: SidebarConfig = {
@@ -108,7 +60,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 		title: 'Hệ thống quản lý học viên',
 		subtitle: 'Trường Cao đẳng hậu cần 2',
 		showCustomContent: true,
-		defaultOpenGroups: false
+		defaultOpenGroups: true
 	}
 
 	const renderProps: SidebarRenderProps = {
@@ -118,7 +70,11 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 			const isCollapsed = state === 'collapsed'
 
 			return (
-				<Link to={item.url} className='flex items-center gap-3 w-full'>
+				<Link
+					to={item.url}
+					state={item.metadata}
+					className='flex items-center gap-3 w-full'
+				>
 					{Icon && <Icon className='w-5 h-5' />}
 					{!isCollapsed && <span>{item.title}</span>}
 				</Link>
@@ -128,12 +84,12 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 
 	return (
 		<GenericSidebar
+			{...props}
 			data={navigationData}
 			config={config}
 			renderProps={renderProps}
-			// isLoading={isLoadingUnits}
+			isLoading={isCourseCategoryLoading}
 			loadingComponent={<AppSidebarSkeleton />}
-			{...props}
 		/>
 	)
 }
