f0e0c8625247c50c91754a213a325a253ac13332
nomorechokedboy <haiduongit@protonmail.com>
2025-09-28 09:20:18 +0700

    feat(sms-api): impl find one course

--- apps/sms-api/internal/courses/mysql_repo.go
+++ apps/sms-api/internal/courses/mysql_repo.go
@@ -65,3 +65,51 @@ func (r *repository) Find(
 
 	return &entities.GetUsersCoursesResponse{Data: resp}, nil
 }
+
+func (r *repository) FindOne(
+	ctx context.Context,
+	req *entities.FindOneCourseParams,
+) (*entities.Course, error) {
+	if req == nil {
+		return nil, fmt.Errorf("Request is nil")
+	}
+
+	resp := &entities.Course{}
+	query := r.db.WithContext(ctx).
+		Select(
+			"c.id AS id",
+			"c.shortname AS shortname",
+			"c.fullname AS fullname",
+			"c.idnumber AS idnumber",
+			"c.visible AS visible",
+			"c.summary AS summary",
+			"c.summaryformat AS summaryformat",
+			"c.format AS format",
+			"c.showgrades AS showgrades",
+			"c.lang AS lang",
+			"c.enablecompletion AS enablecompletion",
+			"c.category AS category",
+			"c.startdate AS startdate",
+			"c.enddate AS enddate",
+			"c.marker AS marker",
+			"c.timemodified AS timemodified",
+		).
+		Distinct(true).
+		From("mdl_course c").
+		InnerJoin("mdl_context ctx", dbx.NewExp("ctx.instanceid = c.id AND ctx.contextlevel = 50")).
+		InnerJoin("mdl_role_assignments ra", dbx.NewExp("ra.contextid = ctx.id")).
+		InnerJoin("mdl_role r", dbx.NewExp("r.id = ra.roleid")).
+		Where(dbx.And(
+			dbx.HashExp{"ra.userid": req.UserId},
+			dbx.HashExp{"c.id": req.Id},
+			dbx.NewExp("r.archetype IN ('editingteacher', 'teacher')"),
+			dbx.NewExp("c.id != 1"),
+		)).
+		OrderBy("c.fullname")
+
+	if err := query.One(resp); err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
