5a1f2db106631f7dfb459bcdd10c9f54c1dd526e
nomorechokedboy <haiduongit@protonmail.com>
2025-10-01 16:17:07 +0700

    feat(api): impl auto migrate

--- apps/api/database.ts
+++ apps/api/database.ts
@@ -1,19 +1,32 @@
-import { drizzle } from 'drizzle-orm/libsql/node';
-import { createClient } from '@libsql/client';
-import * as schema from './schema';
-import { Logger } from 'drizzle-orm';
-import log from 'encore.dev/log';
+import { drizzle } from 'drizzle-orm/libsql/node'
+import { createClient } from '@libsql/client'
+import * as schema from './schema'
+import { Logger } from 'drizzle-orm'
+import log from 'encore.dev/log'
+import { migrate } from 'drizzle-orm/libsql/migrator'
 
 class AppDBLogger implements Logger {
-        logQuery(query: string, params: unknown[]): void {
-                log.trace(`Query info: ${query}`, { params });
-        }
+	logQuery(query: string, params: unknown[]): void {
+		log.trace(`Query info: ${query}`, { params })
+	}
 }
 
-const client = createClient({ url: 'file:local.db' });
+const client = createClient({ url: 'file:local.db' })
 
-const orm = drizzle({ schema, client, logger: new AppDBLogger() });
+const orm = drizzle({ schema, client, logger: new AppDBLogger() })
 
-export type DrizzleDatabase = typeof orm;
+async function autoMigrate() {
+	try {
+		await migrate(orm, { migrationsFolder: './migrations' }) // Specify your migrations folder
+		console.log('Migrations applied successfully!')
+	} catch (error) {
+		console.error('Error applying migrations:', error)
+		process.exit(1)
+	}
+}
 
-export default orm;
+autoMigrate()
+
+export type DrizzleDatabase = typeof orm
+
+export default orm
