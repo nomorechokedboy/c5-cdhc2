76155bab0586654e1add7d62e517e7d8db5aa7d2
nomorechokedboy <haiduongit@protonmail.com>
2025-10-02 20:15:00 +0700

    chore(web): format file

--- apps/web/src/components/family-step.tsx
+++ apps/web/src/components/family-step.tsx
@@ -1,49 +1,49 @@
-import ChildrenInfo from './children-info';
+import ChildrenInfo from './children-info'
 
 export interface FamilyStepProps {
-        form: any;
+	form: any
 }
 
 export default function FamilyStep({ form }: FamilyStepProps) {
-        return (
-                <div className="space-y-8">
-                        <div className="space-y-6">
-                                <h3 className="text-lg font-semibold border-b border-border pb-2">
-                                        Thông tin về vợ/chồng
-                                </h3>
+	return (
+		<div className='space-y-8'>
+			<div className='space-y-6'>
+				<h3 className='text-lg font-semibold border-b border-border pb-2'>
+					Thông tin về vợ/chồng
+				</h3>
 
-                                {/* Spouse Name and Phone - Two Columns */}
-                                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                                        <form.AppField name="spouseName">
-                                                {(field: any) => (
-                                                        <field.TextField label="Tên vợ/chồng" />
-                                                )}
-                                        </form.AppField>
-                                        <form.AppField name="spouseDob">
-                                                {(field: any) => (
-                                                        <field.DatePicker label="Ngày sinh của vợ/chồng" />
-                                                )}
-                                        </form.AppField>
-                                </div>
+				{/* Spouse Name and Phone - Two Columns */}
+				<div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+					<form.AppField name='spouseName'>
+						{(field: any) => (
+							<field.TextField label='Tên vợ/chồng' />
+						)}
+					</form.AppField>
+					<form.AppField name='spouseDob'>
+						{(field: any) => (
+							<field.DatePicker label='Ngày sinh của vợ/chồng' />
+						)}
+					</form.AppField>
+				</div>
 
-                                {/* spouse Job - Full Width */}
-                                <div className="grid grid-cols-2 gap-6">
-                                        <form.AppField name="spousePhoneNumber">
-                                                {(field: any) => (
-                                                        <field.TextField label="Số điện thoại vợ/chồng" />
-                                                )}
-                                        </form.AppField>
-                                        <form.AppField name="spouseJob">
-                                                {(field: any) => (
-                                                        <field.TextField label="Nghề nghiệp vợ/chồng" />
-                                                )}
-                                        </form.AppField>
-                                </div>
-                        </div>
+				{/* spouse Job - Full Width */}
+				<div className='grid grid-cols-2 gap-6'>
+					<form.AppField name='spousePhoneNumber'>
+						{(field: any) => (
+							<field.TextField label='Số điện thoại vợ/chồng' />
+						)}
+					</form.AppField>
+					<form.AppField name='spouseJob'>
+						{(field: any) => (
+							<field.TextField label='Nghề nghiệp vợ/chồng' />
+						)}
+					</form.AppField>
+				</div>
+			</div>
 
-                        <div className="space-y-6">
-                                <ChildrenInfo form={form} />
-                        </div>
-                </div>
-        );
+			<div className='space-y-6'>
+				<ChildrenInfo form={form} />
+			</div>
+		</div>
+	)
 }
