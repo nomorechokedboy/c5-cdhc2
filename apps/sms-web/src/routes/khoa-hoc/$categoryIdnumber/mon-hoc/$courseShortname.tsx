815a7f3c3e4bcd23f3321e5911b7611dca3387fe
nomorechokedboy <haiduongit@protonmail.com>
2025-10-13 10:40:11 +0700

    feat(sms-web): impl grades table with api data

--- apps/sms-web/src/routes/khoa-hoc/$categoryIdnumber/mon-hoc/$courseShortname.tsx
+++ apps/sms-web/src/routes/khoa-hoc/$categoryIdnumber/mon-hoc/$courseShortname.tsx
@@ -1,6 +1,9 @@
+import { CourseApi } from '@/api'
 import CourseDetails from '@/components/course-details'
 import ProtectedRoute from '@/components/ProtectedRoute'
-import { createFileRoute } from '@tanstack/react-router'
+import { Course } from '@/types'
+import { useQuery } from '@tanstack/react-query'
+import { createFileRoute, useRouterState } from '@tanstack/react-router'
 
 export const Route = createFileRoute(
 	'/khoa-hoc/$categoryIdnumber/mon-hoc/$courseShortname'
@@ -10,10 +13,26 @@ export const Route = createFileRoute(
 
 function RouteComponent() {
 	const { courseShortname, categoryIdnumber } = Route.useParams()
+	const course = useRouterState({
+		select(s) {
+			return s.location.state.course
+		}
+	})
+	const {
+		data: courseDetails = Course.DefaultCourse(),
+		isLoading: isLoadingCourseDetails
+	} = useQuery({
+		queryKey: ['courseDetails', course?.id],
+		queryFn: () =>
+			CourseApi.GetCourseDetails({ id: course?.id ?? 0 }).then(
+				Course.From
+			)
+	})
+	console.log({ courseDetails })
 
 	return (
 		<ProtectedRoute>
-			<CourseDetails />
+			<CourseDetails data={courseDetails} />
 		</ProtectedRoute>
 	)
 }
