09cfcbfb04f239ad47a64e940e5186dbb30a84af
nomorechokedboy <haiduongit@protonmail.com>
2025-10-13 10:31:07 +0700

    feat(sms-web): impl course, student types

--- apps/sms-web/src/types/index.ts
+++ apps/sms-web/src/types/index.ts
@@ -1,4 +1,4 @@
-import type { entities } from '@/api/client'
+import type { entities, mdlapi } from '@/api/client'
 
 export type AppToken = {
 	accessToken: string
@@ -34,6 +34,63 @@ export class CourseCategory {
 			idnumber ?? '',
 			visible ?? false,
 			timemodified ?? Date.now()
+		)
+	}
+}
+
+export class Student {
+	constructor(
+		readonly id: number,
+		readonly name: string,
+		readonly grades: Record<string, number>
+	) {}
+
+	public static From({ id, fullname, grades }: mdlapi.Student): Student {
+		const studentGrades: Record<string, number> = {}
+		for (const grade of grades) {
+			studentGrades[grade.modulename] = grade.grade
+		}
+		return new Student(id, fullname, studentGrades)
+	}
+}
+
+export class Course {
+	constructor(
+		readonly id: number,
+		readonly title: string,
+		readonly description: string,
+		readonly startDate: string,
+		readonly endDate: string,
+		readonly gradeCategories: string[],
+		readonly students: Student[]
+	) {}
+
+	public static DefaultCourse() {
+		return new Course(0, '', '', 'N/A', 'N/A', [], [])
+	}
+
+	public static From(
+		resp: mdlapi.GetCourseGradesResponse | undefined
+	): Course {
+		if (resp === undefined) {
+			return this.DefaultCourse()
+		}
+
+		const {
+			course: { id, fullname },
+			modules,
+			students
+		} = resp
+		const courseStudents = students.map(Student.From)
+		const gradeCategories = modules.map((m) => m.name)
+		return new Course(
+			id,
+			fullname,
+			'',
+			'N/A',
+			'N/A',
+			gradeCategories,
+			courseStudents
 		)
 	}
 }
