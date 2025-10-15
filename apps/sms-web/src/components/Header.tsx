e722d6fccfd86c81a69f9ee4673c501d64a74534
nomorechokedboy <haiduongit@protonmail.com>
2025-10-15 07:39:34 +0700

    feat(sms-web): impl sticky header

--- apps/sms-web/src/components/Header.tsx
+++ apps/sms-web/src/components/Header.tsx
@@ -1,29 +1,66 @@
-import { Link } from '@tanstack/react-router'
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator
+} from '@repo/ui/components/ui/breadcrumb'
+import { Separator } from '@repo/ui/components/ui/separator'
+import { SidebarTrigger } from '@repo/ui/components/ui/sidebar'
+import { Link, useLocation } from '@tanstack/react-router'
+import { Fragment } from 'react/jsx-runtime'
 
 export default function Header() {
+	const location = useLocation()
+	const path = location.pathname
+	const segments = path.split('/').filter(Boolean)
+
+	const breadcrumbItems = [
+		{ label: 'Trang chủ', href: '/' },
+		...segments.map((seg, idx) => ({
+			label: decodeURIComponent(seg),
+			href: '/' + segments.slice(0, idx + 1).join('/')
+		}))
+	]
+
 	return (
-		<header className='p-2 flex gap-2 bg-white text-black justify-between'>
-			<nav className='flex flex-row'>
-				<div className='px-2 font-bold'>
-					<Link to='/'>Home</Link>
-				</div>
-
-				<div className='px-2 font-bold'>
-					<Link to='/demo/form/simple'>Simple Form</Link>
-				</div>
-
-				<div className='px-2 font-bold'>
-					<Link to='/demo/form/address'>Address Form</Link>
-				</div>
-
-				<div className='px-2 font-bold'>
-					<Link to='/demo/table'>TanStack Table</Link>
-				</div>
-
-				<div className='px-2 font-bold'>
-					<Link to='/demo/tanstack-query'>TanStack Query</Link>
-				</div>
-			</nav>
+		<header className='flex flex-row items-center justify-between h-16 shrink-0 gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 sticky top-0 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-20 border-b'>
+			<div className='flex items-center gap-2 px-4'>
+				<SidebarTrigger className='-ml-1' />
+				<Separator
+					orientation='vertical'
+					className='mr-2 data-[orientation=vertical]:h-4'
+				/>
+				<Breadcrumb>
+					<BreadcrumbList>
+						{breadcrumbItems.map((item, idx) => (
+							<Fragment key={item.href}>
+								<BreadcrumbItem>
+									{idx < breadcrumbItems.length - 1 ? (
+										<BreadcrumbLink>
+											<Link to={item.href}>
+												{item.label}
+											</Link>
+										</BreadcrumbLink>
+									) : (
+										<BreadcrumbPage>
+											{item.label}
+										</BreadcrumbPage>
+									)}
+								</BreadcrumbItem>
+								{idx < breadcrumbItems.length - 1 && (
+									<BreadcrumbSeparator />
+								)}
+							</Fragment>
+						))}
+					</BreadcrumbList>
+				</Breadcrumb>
+			</div>
+			<div className='px-4 flex items-center gap-5'>
+				{/* <NotificationBell />
+                <UserNav /> */}
+			</div>
 		</header>
 	)
 }
