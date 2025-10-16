be100c51322ecad4e234af8bdadac906207467ab
nomorechokedboy <haiduongit@protonmail.com>
2025-10-16 09:33:54 +0700

    feat(sms-api): add new fields into course details

--- apps/sms-api/internal/mdlapi/coursegrades.go
+++ apps/sms-api/internal/mdlapi/coursegrades.go
@@ -2,7 +2,60 @@ package mdlapi
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 )
+
+type ExamType int
+
+func (t *ExamType) UnmarshalJSON(data []byte) error {
+	var s string
+	if err := json.Unmarshal(data, &s); err != nil {
+		return err
+	}
+
+	switch s {
+	case "15M":
+		*t = Exam15M
+	case "1T":
+		*t = Exam45M
+	case "Thi":
+		*t = ExamFinal
+	default:
+		return errors.New("invalid GradeType: " + s)
+	}
+
+	return nil
+}
+
+func (t *ExamType) MarshalJSON() ([]byte, error) {
+	return json.Marshal(t.String())
+}
+
+const (
+	Exam15M ExamType = iota
+	Exam45M
+	ExamFinal
+)
+
+var (
+	ExamTypeMap = map[ExamType]string{
+		Exam15M:   "15P",
+		Exam45M:   "1T",
+		ExamFinal: "Thi",
+	}
+	ReverseExamTypeMap = map[string]ExamType{
+		"15P": Exam15M,
+		"1T":  Exam45M,
+		"Thi": ExamFinal,
+	}
+	_ json.Marshaler   = (*ExamType)(nil)
+	_ json.Unmarshaler = (*ExamType)(nil)
+)
+
+func (t ExamType) String() string {
+	return ExamTypeMap[t]
+}
 
 type GetCourseGradesRequest struct {
 	CourseId int64 `json:"courseid"`
@@ -10,20 +63,30 @@ type GetCourseGradesRequest struct {
 
 type GetCourseGradesResponse struct {
 	Course struct {
-		ID       int    `json:"id"`
-		Fullname string `json:"fullname"`
+		ID           int    `json:"id"`
+		Fullname     string `json:"fullname"`
+		Shortname    string `json:"shortname"`
+		IDNumber     string `json:"idnumber"`
+		Visible      int    `json:"visible"`
+		Summary      string `json:"summary"`
+		Category     int    `json:"category"`
+		StartDate    int64  `json:"startdate"`
+		EndDate      int64  `json:"enddate"`
+		Marker       int    `json:"marker"`
+		TimeModified int64  `json:"timemodified"`
 	} `json:"course"`
 	Modules  []Module  `json:"modules"`
 	Students []Student `json:"students"`
 }
 
 type Module struct {
-	ID       int     `json:"id"`
-	Cmid     int     `json:"cmid"`
-	Name     string  `json:"name"`
-	Type     string  `json:"type"`
-	Grademin float64 `json:"grademin"`
-	Grademax float64 `json:"grademax"`
+	ID       int       `json:"id"`
+	Cmid     int       `json:"cmid"`
+	Name     string    `json:"name"`
+	Type     string    `json:"type"`
+	Grademin float64   `json:"grademin"`
+	Grademax float64   `json:"grademax"`
+	ExamType *ExamType `json:"examtype"`
 }
 
 type Student struct {
