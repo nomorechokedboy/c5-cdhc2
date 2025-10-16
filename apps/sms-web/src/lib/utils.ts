0e521da38bbe021b6f762fb11c29687609cafa71
nomorechokedboy <haiduongit@protonmail.com>
2025-10-16 09:38:30 +0700

    feat(sms-web): add dayjs and format date util fn

--- apps/sms-web/src/lib/utils.ts
+++ apps/sms-web/src/lib/utils.ts
@@ -1,6 +1,19 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import dayjs from 'dayjs'
+import 'dayjs/locale/vi'
+
+dayjs.locale('vi')
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
+}
+
+export const formatDate = (timestamp: string | number) => {
+	let date = dayjs(timestamp)
+	if (typeof timestamp === 'number') {
+		date = dayjs.unix(timestamp)
+	}
+
+	return date.format('D MMMM, YYYY')
 }
