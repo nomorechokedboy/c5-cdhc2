54d7d51a771e5e0ea4e02ae01480f74a46e41fd5
dangazdada <etyuuia@gmail.com>
2025-11-10 20:26:41 +0700

    feat: add  create user with  field isSuperUser

--- apps/web/src/hooks/useAllUnitsData.ts
+++ apps/web/src/hooks/useAllUnitsData.ts
@@ -2,6 +2,7 @@ import { GetAllUnits } from '@/api'
 import { useQuery } from '@tanstack/react-query'
 
 export default function useAllUnitsData() {
+	console.log('Render useAllUnitsData')
 	return useQuery({
 		queryKey: ['allUnits'],
 		queryFn: () => GetAllUnits()
