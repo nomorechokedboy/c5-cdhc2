fa6cfaa60cee642b263284f1c0fbeebe21acae32
nomorechokedboy <haiduongit@protonmail.com>
2025-11-16 09:57:40 +0700

    chore(sms-web): adjust grade color

--- apps/sms-web/src/components/grade-table/student-grades.tsx
+++ apps/sms-web/src/components/grade-table/student-grades.tsx
@@ -52,8 +52,8 @@ const calculateFinalGrade = (studentGrades: Record<string, Grade>) => {
 }
 
 const getGradeColor = (average: number) => {
-	if (average >= 9) return 'bg-pink-100 text-pink-800 border-pink-200'
-	if (average >= 8) return 'bg-blue-100 text-blue-800 border-blue-200'
+	if (average >= 9) return 'bg-sky-100 text-sky-800 border-sky-200'
+	if (average >= 8) return 'bg-teal-100 text-teal-800 border-teal-200'
 	if (average >= 6) return 'bg-green-100 text-green-800 border-green-200'
 	if (average >= 5) return 'bg-orange-100 text-orange-800 border-orange-200'
 	return 'bg-red-100 text-red-800 border-red-200'
