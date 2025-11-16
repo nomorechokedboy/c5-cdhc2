9dafda6abf6d938b5b5af69d8cff106973db7565
nomorechokedboy <haiduongit@protonmail.com>
2025-11-16 09:50:33 +0700

    chore(sms-web): adjust the grade color

--- apps/sms-web/src/components/grade-table/student-grades.tsx
+++ apps/sms-web/src/components/grade-table/student-grades.tsx
@@ -52,9 +52,9 @@ const calculateFinalGrade = (studentGrades: Record<string, Grade>) => {
 }
 
 const getGradeColor = (average: number) => {
-	if (average >= 9) return 'bg-green-100 text-green-800 border-green-200'
+	if (average >= 9) return 'bg-pink-100 text-pink-800 border-pink-200'
 	if (average >= 8) return 'bg-blue-100 text-blue-800 border-blue-200'
-	if (average >= 7) return 'bg-yellow-100 text-yellow-800 border-yellow-200'
+	if (average >= 6) return 'bg-green-100 text-green-800 border-green-200'
 	if (average >= 5) return 'bg-orange-100 text-orange-800 border-orange-200'
 	return 'bg-red-100 text-red-800 border-red-200'
 }
--- apps/sms-web/src/env.ts
+++ apps/sms-web/src/env.ts
@@ -15,7 +15,7 @@ export const env = createEnv({
 	client: {
 		VITE_APP_TITLE: z.string().min(1).optional(),
 		VITE_CLIENT_ID: z.string().nonempty(),
-		VITE_OAUTH2_URL: z.string().default('http://localhost:8083'),
+		VITE_OAUTH2_URL: z.string().default('http://localhost:8081'),
 		VITE_TOKEN_URI: z.string().default('local/oauth2/login.php'),
 		VITE_REDIRECT_URI: z.string().default('oauth2/callback'),
 		VITE_API_URL: z.string().default('http://localhost:4000')
