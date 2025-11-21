52e7bbdde2bb5878dbd94a3b780786a37d576e8a
dangazdada <etyuuia@gmail.com>
2025-11-21 20:58:29 +0700

    feat: add list user Ui

--- apps/web/src/routes/list-user.tsx
+++ apps/web/src/routes/list-user.tsx
@@ -12,12 +12,10 @@ export const Route = createFileRoute('/list-user')({
 })
 
 function RouteComponent() {
-
 	// const [studentParams, setStudentParams] =
 	// 	React.useState<StudentQueryParams>({
 	// 		isEthnicMinority: true
 	// 	})
-
 	return (
 		<ProtectedRoute>
 			<SidebarInset>
@@ -25,9 +23,7 @@ function RouteComponent() {
 					<div className='flex items-center justify-between space-y-2'>
 						<div>
 							<h2 className='text-2xl font-bold tracking-tight'>
-
 								Danh sách người dùng
-
 							</h2>
 						</div>
 					</div>
