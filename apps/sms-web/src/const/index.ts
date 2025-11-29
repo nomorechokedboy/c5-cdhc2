6309d9fbe6a30d56ddde32a8285ab4f395f7465c
nomorechokedboy <haiduongit@protonmail.com>
2025-11-29 09:00:06 +0700

    fix(sms-web): debug sms web api url

--- apps/sms-web/src/const/index.ts
+++ apps/sms-web/src/const/index.ts
@@ -1,4 +1,5 @@
 import { Local } from '@/api/client'
 import { env } from '@/env'
 
-export const ApiUrl = import.meta.env.DEV === true ? Local : `/api`
+export const ApiUrl =
+	import.meta.env.DEV === true ? Local : `${env.VITE_API_URL}/api`
