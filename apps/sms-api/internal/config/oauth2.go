485c3a60d393e92ebb0917f6ff524f73669ec00b
nomorechokedboy <haiduongit@protonmail.com>
2025-11-29 09:54:28 +0700

    fix(sms-api): impl dynamic redirect url

--- apps/sms-api/internal/config/oauth2.go
+++ apps/sms-api/internal/config/oauth2.go
@@ -10,6 +10,7 @@ type Oauth2Config struct {
 	OriginUrl    string `env:"ORIGIN_URL"    json:"origin_url"`
 	AuthUrl      string `env:"AUTH_URL"      json:"auth_url"`
 	TokenUrl     string `env:"TOKEN_URL"     json:"token_url"`
+	RedirectURL  string `env:"REDIRECT_URL"  json:"redirect_url"  env-default:"http://localhost:4000/oauth2/callback"`
 }
 
 var _ slog.LogValuer = (*Oauth2Config)(nil)
@@ -21,5 +22,6 @@ func (c *Oauth2Config) LogValue() slog.Value {
 		slog.String("origin_url", c.OriginUrl),
 		slog.String("auth_url", c.AuthUrl),
 		slog.String("token_url", c.TokenUrl),
+		slog.String("redirect_url", c.RedirectURL),
 	)
 }
