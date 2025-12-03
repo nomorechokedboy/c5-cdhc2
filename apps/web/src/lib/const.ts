5371906bb2f069222f3f3457fd8a3bd8c6b113cf
nomorechokedboy <haiduongit@protonmail.com>
2025-12-03 15:06:15 +0700

    feat: adjust web repo api url

--- apps/web/src/lib/const.ts
+++ apps/web/src/lib/const.ts
@@ -2,4 +2,4 @@ import { Local } from '@/api/client'
 import { env } from '@/env'
 
 export const ApiUrl =
-	import.meta.env.DEV === true ? Local : `${env.VITE_API_URL}/api`
+	import.meta.env.DEV === true ? Local : `${env.VITE_API_URL}`
