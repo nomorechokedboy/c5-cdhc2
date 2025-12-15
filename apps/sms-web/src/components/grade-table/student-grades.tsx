a7889d47c121ff732d282c9bfe2276df1bd8a388
nomorechokedboy <haiduongit@protonmail.com>
2025-12-15 08:44:37 +0700

    feat(sms-web): add conditional grade col

--- apps/sms-web/src/components/grade-table/student-grades.tsx
+++ apps/sms-web/src/components/grade-table/student-grades.tsx
@@ -19,29 +19,44 @@ export default function StudentGrades({
 	gradeCategories,
 	onGradeSave
 }: StudentGradesProps) {
-	const finalGrade = calculateFinalGrade(student.grades)
+	const { conditionalGrade, finalGrade } = calculateFinalGrade(student.grades)
 
 	return (
 		<TableRow key={student.id} className='hover:bg-muted/30'>
 			<TableCell className='font-medium text-foreground'>
 				{student.name}
 			</TableCell>
-			{gradeCategories.map((category) => {
+			{gradeCategories.map((category, idx) => {
 				const isHighlighted =
 					bulkEditMode === 'all-grades' ||
 					(bulkEditMode === 'single-category' &&
 						bulkEditCategory?.value === category.value)
 
 				return (
-					<TableCell key={category.value} className='text-center'>
-						<EditableGradeCell
-							studentId={student.id}
-							category={category.value}
-							value={student.grades[category.label]?.grade || 0}
-							isHighlighted={isHighlighted}
-							onSave={onGradeSave}
-						/>
-					</TableCell>
+					<>
+						<TableCell key={category.value} className='text-center'>
+							<EditableGradeCell
+								studentId={student.id}
+								category={category.value}
+								value={
+									student.grades[category.label]?.grade || 0
+								}
+								isHighlighted={isHighlighted}
+								onSave={onGradeSave}
+							/>
+						</TableCell>
+						{gradeCategories.length > 2 &&
+							idx === gradeCategories.length - 2 && (
+								<TableCell className='text-center'>
+									<Badge
+										variant='outline'
+										className={`font-semibold ${getGradeColor(finalGrade)}`}
+									>
+										{conditionalGrade.toFixed(2)}
+									</Badge>
+								</TableCell>
+							)}
+					</>
 				)
 			})}
 			<TableCell className='text-center'>
