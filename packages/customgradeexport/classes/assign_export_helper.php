4c3a5940862cbe714ba659b4f552de323250fcdd
nomorechokedboy <haiduongit@protonmail.com>
2025-12-16 16:26:07 +0700

    fix: fix customgradeexport error

--- packages/customgradeexport/classes/assign_export_helper.php
+++ packages/customgradeexport/classes/assign_export_helper.php
@@ -1,6 +1,7 @@
 <?php
+
 /**
- * Assignment export helper class - Enhanced with DOCX support
+ * Assignment export helper class - Enhanced with DOCX and template support
  *
  * @package    local_customgradeexport
  * @copyright  2024 Your Name
@@ -17,20 +18,21 @@ require_once($CFG->dirroot . '/mod/assign/locallib.php');
 /**
  * Helper class for exporting assignment grades
  */
-class assign_export_helper {
-    
+class assign_export_helper
+{
+
     /** @var stdClass Assignment instance */
     protected $assignment;
-    
+
     /** @var stdClass Course module */
     protected $cm;
-    
+
     /** @var stdClass Course */
     protected $course;
-    
+
     /** @var context_module Context */
     protected $context;
-    
+
     /**
      * Constructor
      *
@@ -38,70 +40,113 @@ class assign_export_helper {
      * @param stdClass $cm Course module
      * @param stdClass $course Course
      */
-    public function __construct($assignment, $cm, $course) {
+    public function __construct($assignment, $cm, $course)
+    {
         $this->assignment = $assignment;
         $this->cm = $cm;
         $this->course = $course;
         $this->context = \context_module::instance($cm->id);
     }
-    
+
     /**
      * Export assignment grades to Excel
+     *
+     * @param string|null $templatePath Optional path to template file
      */
-    public function export_grades() {
+    public function export_grades($templatePath = null)
+    {
         global $CFG;
-        
+
         require_capability('mod/assign:grade', $this->context);
         require_capability('local/customgradeexport:export', $this->context);
-        
+
         // Get assignment instance
         $assign = new \assign($this->context, $this->cm, $this->course);
-        
+
         // Get submissions and grades
         $data = $this->prepare_export_data($assign);
-        
-        // Send download
-        $this->send_excel_download($data);
+
+        if ($templatePath && file_exists($templatePath)) {
+            // Export with template
+            $this->export_with_excel_template($data, $templatePath);
+        } else {
+            // Send standard download
+            $this->send_excel_download($data);
+        }
     }
-    
+
+    /**
+     * Export assignment grades to Excel with template
+     *
+     * @param string $templatePath Path to template file
+     */
+    public function export_grades_excel($templatePath)
+    {
+        $this->export_grades($templatePath);
+    }
+
     /**
      * Export assignment grades to DOCX
+     *
+     * @param string|null $templatePath Optional path to template file
      */
-    public function export_grades_docx() {
+    public function export_grades_docx($templatePath = null)
+    {
         global $CFG;
-        
+
         require_capability('mod/assign:grade', $this->context);
         require_capability('local/customgradeexport:export', $this->context);
-        
+
         // Get assignment instance
         $assign = new \assign($this->context, $this->cm, $this->course);
-        
+
         // Get submissions and grades
         $data = $this->prepare_export_data($assign);
-        
-        // Check if template exists
-        $templatePath = template_processor::get_template_path('assign');
-        
-        $filename = clean_filename($this->course->shortname . '_' . 
-                                   $this->assignment->name . '_grades.docx');
-        
-        if ($templatePath) {
-            // Use template
+
+        $filename = clean_filename($this->course->shortname . '_' .
+            $this->assignment->name . '_grades.docx');
+
+        if ($templatePath && file_exists($templatePath)) {
+            // Use provided template
             $this->export_with_template($data, $templatePath, $filename);
         } else {
             // Use default table format
             docx_exporter::export_table($data, $filename);
         }
     }
-    
+
     /**
-     * Export using template
+     * Export using Excel template
+     *
+     * @param array $data Export data
+     * @param string $templatePath Template file path
+     */
+    protected function export_with_excel_template($data, $templatePath)
+    {
+        // Prepare variables for template
+        $variables = [
+            'coursename' => $this->course->fullname,
+            'activityname' => $this->assignment->name,
+            'exportdate' => userdate(time(), '%d/%m/%Y'),
+            'exporttime' => userdate(time(), '%H:%M:%S'),
+        ];
+
+        $filename = clean_filename($this->course->shortname . '_' .
+            $this->assignment->name . '_grades.xlsx');
+
+        // Use Excel template processor
+        excel_template_processor::export_from_template($templatePath, $variables, $data, $filename);
+    }
+
+    /**
+     * Export using DOCX template
      *
      * @param array $data Export data
      * @param string $templatePath Template file path
      * @param string $filename Output filename
      */
-    protected function export_with_template($data, $templatePath, $filename) {
+    protected function export_with_template($data, $templatePath, $filename)
+    {
         // Prepare variables for template
         $variables = [
             'coursename' => $this->course->fullname,
@@ -109,20 +154,21 @@ class assign_export_helper {
             'exportdate' => date('Y-m-d'),
             'exporttime' => date('H:i:s'),
         ];
-        
+
         // Export
         docx_exporter::export_from_template($templatePath, $variables, $data, $filename);
     }
-    
+
     /**
      * Prepare data for export
      *
      * @param assign $assign Assignment instance
      * @return array 2D array of export data
      */
-    protected function prepare_export_data($assign) {
+    protected function prepare_export_data($assign)
+    {
         global $DB;
-        
+
         // Headers
         $headers = [
             'First name',
@@ -140,44 +186,44 @@ class assign_export_helper {
             'Grader',
             'Feedback comments',
         ];
-        
+
         $data = [];
         $data[] = $headers;
-        
+
         // Get all participants with their org data
         $participants = $assign->list_participants(null, true);
-        
+
         if (empty($participants)) {
             return $data; // Return just headers if no participants
         }
-        
+
         // Get user org data in bulk for efficiency
         $userids = array_keys($participants);
         list($insql, $params) = $DB->get_in_or_equal($userids);
         $userorgdata = $DB->get_records_select('user', "id $insql", $params, '', 'id, institution, department');
-        
+
         foreach ($participants as $user) {
             // Get submission
             $submission = $assign->get_user_submission($user->id, false);
-            
+
             // Get grade
             $grade = $assign->get_user_grade($user->id, false);
-            
+
             // Get org data
             $orgdata = isset($userorgdata[$user->id]) ? $userorgdata[$user->id] : null;
             $institution = $orgdata && !empty($orgdata->institution) ? $orgdata->institution : '';
             $department = $orgdata && !empty($orgdata->department) ? $orgdata->department : '';
-            
+
             // Get status
             $status = $this->get_submission_status($submission);
-            
+
             // Calculate percentage
             $gradevalue = $grade ? $grade->grade : null;
             $percentage = '';
             if ($gradevalue !== null && $this->assignment->grade > 0) {
                 $percentage = round(($gradevalue / $this->assignment->grade) * 100, 2) . '%';
             }
-            
+
             // Get grader name
             $gradername = '';
             if ($grade && $grade->grader > 0) {
@@ -186,10 +232,10 @@ class assign_export_helper {
                     $gradername = fullname($grader);
                 }
             }
-            
+
             // Get feedback comments
             $feedback = $this->get_feedback_comments($grade);
-            
+
             $row = [
                 $user->firstname,
                 $user->lastname,
@@ -206,24 +252,25 @@ class assign_export_helper {
                 $gradername,
                 $feedback,
             ];
-            
+
             $data[] = $row;
         }
-        
+
         return $data;
     }
-    
+
     /**
      * Get submission status
      *
      * @param stdClass|null $submission Submission record
      * @return string Status string
      */
-    protected function get_submission_status($submission) {
+    protected function get_submission_status($submission)
+    {
         if (!$submission) {
             return 'No submission';
         }
-        
+
         switch ($submission->status) {
             case ASSIGN_SUBMISSION_STATUS_SUBMITTED:
                 return 'Submitted';
@@ -237,46 +284,48 @@ class assign_export_helper {
                 return $submission->status;
         }
     }
-    
+
     /**
      * Get feedback comments
      *
      * @param stdClass|null $grade Grade record
      * @return string Feedback text
      */
-    protected function get_feedback_comments($grade) {
+    protected function get_feedback_comments($grade)
+    {
         global $DB;
-        
+
         if (!$grade) {
             return '';
         }
-        
+
         $feedback = $DB->get_record('assignfeedback_comments', [
             'assignment' => $this->assignment->id,
             'grade' => $grade->id
         ]);
-        
+
         if ($feedback) {
             return strip_tags($feedback->commenttext);
         }
-        
+
         return '';
     }
-    
+
     /**
      * Send Excel file download
      *
      * @param array $data 2D array of export data
      */
-    protected function send_excel_download($data) {
-        $filename = clean_filename($this->course->shortname . '_' . 
-                                   $this->assignment->name . '_grades.xls');
-        
+    protected function send_excel_download($data)
+    {
+        $filename = clean_filename($this->course->shortname . '_' .
+            $this->assignment->name . '_grades.xls');
+
         $workbook = new \MoodleExcelWorkbook('-');
         $workbook->send($filename);
-        
+
         $worksheet = $workbook->add_worksheet('Grades');
-        
+
         // Write data
         $row = 0;
         foreach ($data as $rowdata) {
@@ -287,7 +336,7 @@ class assign_export_helper {
             }
             $row++;
         }
-        
+
         $workbook->close();
         exit;
     }
--- packages/customgradeexport/classes/quiz_export_helper.php
+++ packages/customgradeexport/classes/quiz_export_helper.php
@@ -1,6 +1,7 @@
 <?php
+
 /**
- * Quiz export helper class - Enhanced with DOCX support
+ * Quiz export helper class - Enhanced with DOCX and template support
  *
  * @package    local_customgradeexport
  * @copyright  2024 Your Name
@@ -17,20 +18,21 @@ require_once($CFG->dirroot . '/mod/quiz/locallib.php');
 /**
  * Helper class for exporting quiz grades
  */
-class quiz_export_helper {
-    
+class quiz_export_helper
+{
+
     /** @var stdClass Quiz instance */
     protected $quiz;
-    
+
     /** @var stdClass Course module */
     protected $cm;
-    
+
     /** @var stdClass Course */
     protected $course;
-    
+
     /** @var context_module Context */
     protected $context;
-    
+
     /**
      * Constructor
      *
@@ -38,70 +40,113 @@ class quiz_export_helper {
      * @param stdClass $cm Course module
      * @param stdClass $course Course
      */
-    public function __construct($quiz, $cm, $course) {
+    public function __construct($quiz, $cm, $course)
+    {
         $this->quiz = $quiz;
         $this->cm = $cm;
         $this->course = $course;
         $this->context = \context_module::instance($cm->id);
     }
-    
+
     /**
      * Export quiz grades to Excel
+     *
+     * @param string|null $templatePath Optional path to template file
      */
-    public function export_grades() {
+    public function export_grades($templatePath = null)
+    {
         global $CFG;
-        
+
         require_capability('mod/quiz:viewreports', $this->context);
         require_capability('local/customgradeexport:export', $this->context);
-        
+
         // Get quiz attempts
         $attempts = $this->get_quiz_attempts();
-        
+
         // Prepare export data
         $exportdata = $this->prepare_export_data($attempts);
-        
-        // Send download
-        $this->send_excel_download($exportdata);
+
+        if ($templatePath && file_exists($templatePath)) {
+            // Export with template
+            $this->export_with_excel_template($exportdata, $templatePath);
+        } else {
+            // Send standard download
+            $this->send_excel_download($exportdata);
+        }
     }
-    
+
+    /**
+     * Export quiz grades to Excel with template
+     *
+     * @param string $templatePath Path to template file
+     */
+    public function export_grades_excel($templatePath)
+    {
+        $this->export_grades($templatePath);
+    }
+
     /**
      * Export quiz grades to DOCX
+     *
+     * @param string|null $templatePath Optional path to template file
      */
-    public function export_grades_docx() {
+    public function export_grades_docx($templatePath = null)
+    {
         global $CFG;
-        
+
         require_capability('mod/quiz:viewreports', $this->context);
         require_capability('local/customgradeexport:export', $this->context);
-        
+
         // Get quiz attempts
         $attempts = $this->get_quiz_attempts();
-        
+
         // Prepare export data
         $exportdata = $this->prepare_export_data($attempts);
-        
-        // Check if template exists
-        $templatePath = template_processor::get_template_path('quiz');
-        
-        $filename = clean_filename($this->course->shortname . '_' . 
-                                   $this->quiz->name . '_grades.docx');
-        
-        if ($templatePath) {
-            // Use template
+
+        $filename = clean_filename($this->course->shortname . '_' .
+            $this->quiz->name . '_grades.docx');
+
+        if ($templatePath && file_exists($templatePath)) {
+            // Use provided template
             $this->export_with_template($exportdata, $templatePath, $filename);
         } else {
             // Use default table format
             docx_exporter::export_table($exportdata, $filename);
         }
     }
-    
+
     /**
-     * Export using template
+     * Export using Excel template
+     *
+     * @param array $data Export data
+     * @param string $templatePath Template file path
+     */
+    protected function export_with_excel_template($data, $templatePath)
+    {
+        // Prepare variables for template
+        $variables = [
+            'coursename' => $this->course->fullname,
+            'activityname' => $this->quiz->name,
+            'exportdate' => userdate(time(), '%d/%m/%Y'),
+            'exporttime' => userdate(time(), '%H:%M:%S'),
+        ];
+
+        $filename = clean_filename($this->course->shortname . '_' .
+            $this->quiz->name . '_grades.xlsx');
+
+        // Use Excel template processor
+        excel_template_processor::export_from_template($templatePath, $variables, $data, $filename);
+    }
+
+    /**
+     * Export using DOCX template
      *
      * @param array $data Export data
      * @param string $templatePath Template file path
      * @param string $filename Output filename
      */
-    protected function export_with_template($data, $templatePath, $filename) {
+    protected function export_with_template($data, $templatePath, $filename)
+    {
         // Prepare variables for template
         $variables = [
             'coursename' => $this->course->fullname,
@@ -109,19 +154,20 @@ class quiz_export_helper {
             'exportdate' => date('Y-m-d'),
             'exporttime' => date('H:i:s'),
         ];
-        
+
         // Export
         docx_exporter::export_from_template($templatePath, $variables, $data, $filename);
     }
-    
+
     /**
      * Get all quiz attempts for export with user data
      *
      * @return array Array of attempt records
      */
-    protected function get_quiz_attempts() {
+    protected function get_quiz_attempts()
+    {
         global $DB;
-        
+
         // Get all attempts with user information in one query
         $sql = "SELECT qa.*, 
                        u.id as userid, u.firstname, u.lastname, 
@@ -132,17 +178,18 @@ class quiz_export_helper {
                 WHERE qa.quiz = :quizid 
                   AND qa.preview = 0
                 ORDER BY u.lastname, u.firstname, qa.attempt";
-        
+
         return $DB->get_records_sql($sql, ['quizid' => $this->quiz->id]);
     }
-    
+
     /**
      * Prepare data for export
      *
      * @param array $attempts Array of quiz attempts
      * @return array 2D array of export data
      */
-    protected function prepare_export_data($attempts) {
+    protected function prepare_export_data($attempts)
+    {
         // Headers
         $headers = [
             'First name',
@@ -160,27 +207,27 @@ class quiz_export_helper {
             'Time finished',
             'Time taken',
         ];
-        
+
         $data = [];
         $data[] = $headers;
-        
+
         // Process each attempt
         foreach ($attempts as $attempt) {
             // Calculate grade
             $finalgrade = quiz_rescale_grade($attempt->sumgrades, $this->quiz, false);
-            $percentage = ($this->quiz->grade > 0) ? 
+            $percentage = ($this->quiz->grade > 0) ?
                 round(($finalgrade / $this->quiz->grade) * 100, 2) : 0;
-            
+
             // Calculate time taken
             $timetaken = '';
             if ($attempt->timefinish > 0 && $attempt->timestart > 0) {
                 $duration = $attempt->timefinish - $attempt->timestart;
                 $timetaken = format_time($duration);
             }
-            
+
             // Get state display
             $statedisplay = $this->get_state_display($attempt->state);
-            
+
             $row = [
                 $attempt->firstname,
                 $attempt->lastname,
@@ -197,44 +244,46 @@ class quiz_export_helper {
                 $attempt->timefinish > 0 ? userdate($attempt->timefinish) : '-',
                 $timetaken,
             ];
-            
+
             $data[] = $row;
         }
-        
+
         return $data;
     }
-    
+
     /**
      * Get display text for attempt state
      *
      * @param string $state State code
      * @return string Display text
      */
-    protected function get_state_display($state) {
+    protected function get_state_display($state)
+    {
         $states = [
             'inprogress' => 'In progress',
             'overdue' => 'Overdue',
             'finished' => 'Finished',
             'abandoned' => 'Never submitted',
         ];
-        
+
         return isset($states[$state]) ? $states[$state] : $state;
     }
-    
+
     /**
      * Send Excel file download
      *
      * @param array $data 2D array of export data
      */
-    protected function send_excel_download($data) {
-        $filename = clean_filename($this->course->shortname . '_' . 
-                                   $this->quiz->name . '_grades.xls');
-        
+    protected function send_excel_download($data)
+    {
+        $filename = clean_filename($this->course->shortname . '_' .
+            $this->quiz->name . '_grades.xls');
+
         $workbook = new \MoodleExcelWorkbook('-');
         $workbook->send($filename);
-        
+
         $worksheet = $workbook->add_worksheet('Grades');
-        
+
         // Write data
         $row = 0;
         foreach ($data as $rowdata) {
@@ -245,7 +294,7 @@ class quiz_export_helper {
             }
             $row++;
         }
-        
+
         $workbook->close();
         exit;
     }
