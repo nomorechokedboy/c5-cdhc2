247f73e95752f90209fdde31853aa77bd99f2770
tho076 <thotruong1976@gmail.com>
2026-01-05 20:46:09 +0700

    chore: adjust text

--- apps/sms-web/src/routes/login.tsx
+++ apps/sms-web/src/routes/login.tsx
@@ -87,7 +87,7 @@ export default function Login() {
 									width={24}
 									height={24}
 								/>
-								Đăng nhập bằng Học liệu số
+								Đăng nhập bằng LMS
 							</Button>
 						</CardContent>
 					</Card>
